a01274f6230682069e5fef26a9a383f338b9591c
varmil <fkmks247@gmail.com>
2024-06-27 20:41:48 +0900

    chack caching behaviour v1.1.0

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,14 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!'
+    return 'Hello World! v1.1.0'
+  }
+
+  getFoo(): string {
+    return 'FOOOOO!'
+  }
+
+  getBar(): string {
+    return 'BARRRRR!'
   }
 }
