6958814b6d3c7ddcbe52c943a06b588eed39a22a
varmil <fkmks247@gmail.com>
2024-06-27 20:24:37 +0900

    Add rewrites on hosting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  /**
+   * /api/* にすべてマッピング
+   */
+  app.setGlobalPrefix('api')
+
   await app.listen(process.env.PORT || 15000)
 }
 bootstrap()
