e0363b8db69244d4aa7eafbd37407749d6517b09
varmil <fkmks247@gmail.com>
2024-07-07 13:23:21 +0900

    refactor

--- backend/src/domain/youtube/channel/Channel.entity.ts
+++ backend/src/domain/youtube/channel/Channel.entity.ts
@@ -1,6 +1,8 @@
 import { BrandingSettings } from '@domain/youtube/channel/BrandingSettings'
 import { ChannelStatistics } from '@domain/youtube/channel/ChannelStatistics'
 import { ChannelBasicInfo } from '@domain/youtube/channel/basic-info/ChannelBasicInfo.entity'
+import { Country } from '@domain/youtube/channel/branding-settings/Country'
+import { Keywords } from '@domain/youtube/channel/branding-settings/Keywords'
 
 /**
  * 本当は全チャンネル /v3/channels を１回でも回せば
@@ -9,7 +11,7 @@ import { ChannelBasicInfo } from '@domain/youtube/channel/basic-info/ChannelBasi
  */
 export class Channel {
   public readonly basicInfo: ChannelBasicInfo
-  public readonly statistics?: ChannelStatistics
+  public readonly statistics: ChannelStatistics
   public readonly brandingSettings?: BrandingSettings
 
   constructor(args: {
@@ -18,7 +20,12 @@ export class Channel {
     brandingSettings?: BrandingSettings
   }) {
     this.basicInfo = args.basicInfo
-    this.statistics = args.statistics
-    this.brandingSettings = args.brandingSettings
+    this.statistics = args.statistics ?? new ChannelStatistics({})
+    this.brandingSettings =
+      args.brandingSettings ??
+      new BrandingSettings({
+        keywords: new Keywords([]),
+        country: new Country()
+      })
   }
 }
