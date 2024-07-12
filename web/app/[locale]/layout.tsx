e90c9a94afea428acf7a204e744548a15c50d187
Akihiro Yamamoto <fkmks247@gmail.com>
2024-07-13 00:46:58 +0900

    Add dark,light,system mode (#78)

--- web/app/[locale]/layout.tsx
+++ web/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from 'next-intl'
 import { getMessages, unstable_setRequestLocale } from 'next-intl/server'
 import NextTopLoader from 'nextjs-toploader'
 import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from 'components/styles/ThemeProvider'
 import { locales } from 'config/i18n/locale'
 
 type Props = {
@@ -27,14 +28,21 @@ export default async function LocaleLayout({
   const messages = await getMessages()
 
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID as string} />
       <body>
-        <NextTopLoader color="hsl(var(--primary))" showSpinner={false} />
-        <NextIntlClientProvider messages={messages}>
-          {children}
-          <Toaster />
-        </NextIntlClientProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextTopLoader color="hsl(var(--primary))" showSpinner={false} />
+          <NextIntlClientProvider messages={messages}>
+            {children}
+            <Toaster />
+          </NextIntlClientProvider>
+        </ThemeProvider>
       </body>
     </html>
   )
