f7188cbac7ed7d7afb8f3c7a7e666c513d2ccaa1
varmil <fkmks247@gmail.com>
2024-07-13 21:12:54 +0900

    Hide search bar

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -95,18 +95,20 @@ export default function Header() {
         scroll={false}
       >
         <Logo className="w-6 h-6" />
-        <span className="hidden md:flex text-lg font-bold">{t('title')}</span>
+        <span className="md:flex text-lg font-bold">{t('title')}</span>
       </Link>
 
-      <div className="relative ml-auto flex-1 md:grow-0">
+      {/* Search Icon */}
+      {/* <div className="relative ml-auto flex-1 md:grow-0">
         <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
         <Input
           type="search"
           placeholder="Search..."
           className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
         />
-      </div>
+      </div> */}
 
+      {/* User Icon */}
       {/* <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -133,7 +135,7 @@ export default function Header() {
         </DropdownMenuContent>
       </DropdownMenu> */}
 
-      <div>
+      <div className="relative ml-auto ">
         <ModeToggle />
       </div>
     </header>
