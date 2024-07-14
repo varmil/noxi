3b553746add6aeee2dfa5e7d9f1260e50b96952f
varmil <fkmks247@gmail.com>
2024-07-14 20:07:47 +0900

    Impl Header Sheet

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,11 +1,6 @@
-import {
-  HomeIcon,
-  ShoppingCartIcon,
-  PackageIcon,
-  UsersIcon,
-  LineChartIcon,
-  SearchIcon
-} from 'lucide-react'
+import { PropsWithChildren } from 'react'
+import { SearchIcon, SettingsIcon } from 'lucide-react'
+import Image from 'next/image'
 import { useTranslations } from 'next-intl'
 import { Button } from '@/components/ui/button'
 import {
@@ -19,8 +14,16 @@ import {
 import { Input } from '@/components/ui/input'
 import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet'
 import { ModeToggle } from 'components/ModeToggle'
+import InstagramIcon from 'components/icons/InstagramIcon'
+import PrivacyPolicyIcon from 'components/icons/PrivacyPolicyIcon'
+import TikTokIcon from 'components/icons/TikTokIcon'
+import TwitchIcon from 'components/icons/TwitchIcon'
 import { Link } from 'lib/navigation'
 import Logo from './Logo'
+
+const IconWrapper = ({ children }: PropsWithChildren<{}>) => (
+  <div className="flex justify-center items-center h-8 w-8">{children}</div>
+)
 
 export default function Header() {
   const t = useTranslations('Global')
@@ -37,52 +40,78 @@ export default function Header() {
         <SheetContent side="left" className="sm:max-w-xs">
           <nav className="grid gap-6 text-lg font-medium">
             <Link
-              href="#"
-              className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              href="/"
+              className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-secondary text-lg font-semibold text-primary-foreground md:text-base"
               prefetch={false}
             >
-              <Package2Icon className="h-5 w-5 transition-all group-hover:scale-110" />
-              <span className="sr-only">Acme Inc</span>
+              <Logo className="h-5 w-5 transition-all group-hover:scale-110" />
+              <span className="sr-only">PeakX</span>
             </Link>
             <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-              prefetch={false}
-            >
-              <HomeIcon className="h-5 w-5" />
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-              prefetch={false}
-            >
-              <ShoppingCartIcon className="h-5 w-5" />
-              Orders
-            </Link>
-            <Link
-              href="#"
+              href="/"
               className="flex items-center gap-4 px-2.5 text-foreground"
               prefetch={false}
             >
-              <PackageIcon className="h-5 w-5" />
-              Products
+              <Image
+                src={'/yt_icon_rgb.png'}
+                alt={`YouTube icon`}
+                width={734 / 4}
+                height={518 / 4}
+                className="h-8 w-8 object-contain transition-all group-hover:scale-110"
+                priority={false}
+              />
+              <span className="flex-1">YouTube</span>
+            </Link>
+
+            <Link
+              href="#"
+              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              prefetch={false}
+            >
+              <IconWrapper>
+                <TwitchIcon className="h-6 w-6" />
+              </IconWrapper>
+              <span className="flex-1">Coming soon...</span>
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-4 px-2.5 text-muted-foreground"
+              prefetch={false}
+            >
+              <IconWrapper>
+                <TikTokIcon className="h-6 w-6" />
+              </IconWrapper>
+              <span className="flex-1">Coming soon...</span>
             </Link>
             <Link
               href="#"
               className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
               prefetch={false}
             >
-              <UsersIcon className="h-5 w-5" />
-              Customers
+              <IconWrapper>
+                <InstagramIcon className="h-6 w-6" />
+              </IconWrapper>
+              <span className="flex-1">Coming soon...</span>
             </Link>
             <Link
               href="#"
               className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
               prefetch={false}
             >
-              <LineChartIcon className="h-5 w-5" />
-              Settings
+              <IconWrapper>
+                <SettingsIcon className="h-6 w-6" />
+              </IconWrapper>
+              <span className="flex-1">Settings</span>
+            </Link>
+            <Link
+              href="/youtube/terms-of-use-and-privacy-policy"
+              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+              prefetch={false}
+            >
+              <IconWrapper>
+                <PrivacyPolicyIcon className="h-6 w-6" />
+              </IconWrapper>
+              <span className="flex-1">Terms of Use and Privacy Policy</span>
             </Link>
           </nav>
         </SheetContent>
@@ -158,27 +187,6 @@ function PanelLeftIcon(props) {
     >
       <rect width="18" height="18" x="3" y="3" rx="2" />
       <path d="M9 3v18" />
-    </svg>
-  )
-}
-
-function Package2Icon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z" />
-      <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9" />
-      <path d="M12 3v6" />
     </svg>
   )
 }
