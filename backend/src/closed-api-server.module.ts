651f2af91b16464308747d90a0fc3c180f5b2dcb
varmil <fkmks247@gmail.com>
2024-08-01 17:39:11 +0900

    Create health-check API

--- backend/src/closed-api-server.module.ts
+++ backend/src/closed-api-server.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { CloudSchedulersYoutubeModule as CloudSchedulersYoutubePresentationModule } from '@app/cloud-schedulers/youtube/cloud-schedulers-youtube.module'
+import { HealthModule as HealthPresentationModule } from '@app/health/health.module'
 import { YoutubeModule as YoutubePresentationModule } from '@app/youtube/youtube.module'
 import { AppController } from './presentation-application/app.controller'
 import { AppService } from './presentation-application/app.service'
@@ -11,6 +12,7 @@ import { LibModule } from './presentation-application/lib/lib.module'
     // in only Local, load .env , in other environments, directly embed with Cloud Run
     ConfigModule.forRoot({ ignoreEnvFile: !!process.env.ENV_NAME }),
     LibModule,
+    HealthPresentationModule,
     YoutubePresentationModule,
     CloudSchedulersYoutubePresentationModule
   ],
