04af7b16c1683e447fd0e4a3666bcdcfb74422a4
varmil <fkmks247@gmail.com>
2024-08-04 20:49:02 +0900

    Add backend/libs/domain/hololive/list.ts

---
+++ backend/libs/domain/hololive/list.ts
@@ -0,0 +1,186 @@
+export default [
+  {
+    id: 'UCp6993wxpyDPHUpavwDFqgg',
+    group: 'ホロライブ',
+    title: 'SoraCh. ときのそらチャンネル'
+  },
+  {
+    id: 'UCDqI2jOz0weumE8s7paEk6g',
+    group: 'ホロライブ',
+    title: 'Roboco Ch. - ロボ子'
+  },
+  {
+    id: 'UCFTLzh12_nrtzqBPsTCqenA',
+    group: 'ホロライブ',
+    title: 'アキ・ローゼンタール'
+  },
+  {
+    id: 'UC1CfXB_kRs3C-zaeTG3oGyg',
+    group: 'ホロライブ',
+    title: 'HAACHAMA Ch 赤井はあと'
+  },
+
+  {
+    id: 'UCdn5BQ06XqgXoAxIhbqw5Rg',
+    group: 'ホロライブ',
+    title: 'フブキCh。白上フブキ'
+  },
+  {
+    id: 'UCQ0UDLQCjY0rmuxCDE38FGg',
+    group: 'ホロライブ',
+    title: 'Matsuri Channel 夏色まつり'
+  },
+  {
+    id: 'UC1opHUrw8rvnsadT-iGp7Cg',
+    group: 'ホロライブ',
+    title: 'Aqua Ch. 湊あくあ'
+  },
+  {
+    id: 'UCXTpFs_3PqI41qX2d9tL2Rw',
+    group: 'ホロライブ',
+    title: 'Shion Ch. 紫咲シオン'
+  },
+
+  {
+    id: 'UC7fk0CB07ly8oSl0aqKkqFg',
+    group: 'ホロライブ',
+    title: 'Nakiri Ayame Ch. 百鬼あやめ'
+  },
+  {
+    id: 'UC1suqwovbL1kzsoaZgFZLKg',
+    group: 'ホロライブ',
+    title: 'Choco Ch. 癒月ちょこ'
+  },
+  {
+    id: 'UCvzGlP9oQwU--Y0r9id_jnA',
+    group: 'ホロライブ',
+    title: 'Subaru Ch. 大空スバル'
+  },
+  {
+    id: 'UC0TXe_LYZ4scaW2XMyi5_kw',
+    group: 'ホロライブ',
+    title: 'AZKi Channel'
+  },
+
+  {
+    id: 'UCp-5t9SrOQwXMU7iIjQfARg',
+    group: 'ホロライブ',
+    title: 'Mio Channel 大神ミオ'
+  },
+  {
+    id: 'UC-hM6YJuNYVAmUWxeIr9FeA',
+    group: 'ホロライブ',
+    title: 'Miko Ch. さくらみこ'
+  },
+  {
+    id: 'UCvaTdHTWBGv3MKj3KVqJVCw',
+    group: 'ホロライブ',
+    title: 'Okayu Ch. 猫又おかゆ'
+  },
+  {
+    id: 'UChAnqc_AY5_I3Px5dig3X1Q',
+    group: 'ホロライブ',
+    title: 'Korone Ch. 戌神ころね'
+  },
+
+  {
+    id: 'UC5CwaMl1eIgY8h02uZw7u8A',
+    group: 'ホロライブ',
+    title: 'Suisei Channel'
+  },
+  {
+    id: 'UC1DCedRgGHBdm81E1llLhOQ',
+    group: 'ホロライブ',
+    title: 'Pekora Ch. 兎田ぺこら'
+  },
+  {
+    id: 'UCvInZx9h3jC2JzsIzoOebWg',
+    group: 'ホロライブ',
+    title: '不知火フレア'
+  },
+  {
+    id: 'UCdyqAaZDKHXg4Ahi7VENThQ',
+    group: 'ホロライブ',
+    title: 'Noel Ch. 白銀ノエル'
+  },
+
+  {
+    id: 'UCCzUftO8KOVkV4wQG1vkUvg',
+    group: 'ホロライブ',
+    title: 'Marine Ch. 宝鐘マリン'
+  },
+  {
+    id: 'UCZlDXzGoo7d44bwdNObFacg',
+    group: 'ホロライブ',
+    title: 'Kanata Ch. 天音かなた'
+  },
+  {
+    id: 'UCqm3BQLlJfvkTsX_hvm0UmA',
+    group: 'ホロライブ',
+    title: 'Watame Ch. 角巻わため'
+  },
+  {
+    id: 'UC1uv2Oq6kNxgATlCiez59hw',
+    group: 'ホロライブ',
+    title: 'Towa Ch. 常闇トワ'
+  },
+
+  {
+    id: 'UCa9Y57gfeY0Zro_noHRVrnw',
+    group: 'ホロライブ',
+    title: 'Luna Ch. 姫森ルーナ'
+  },
+  {
+    id: 'UCFKOVgVbGmX65RxO3EtH3iw',
+    group: 'ホロライブ',
+    title: 'Lamy Ch. 雪花ラミィ'
+  },
+  {
+    id: 'UCAWSyEs_Io8MtpY3m-zqILA',
+    group: 'ホロライブ',
+    title: 'Nene Ch. 桃鈴ねね'
+  },
+  {
+    id: 'UCUKD-uaobj9jiqB-VXt71mA',
+    group: 'ホロライブ',
+    title: 'Botan Ch. 獅白ぼたん'
+  },
+
+  {
+    id: 'UCK9V2B22uJYu3N7eR_BT9QA',
+    group: 'ホロライブ',
+    title: 'Polka Ch. 尾丸ポルカ'
+  },
+  {
+    id: 'UCENwRMx5Yh42zWpzURebzTw',
+    group: 'ホロライブ',
+    title: 'Laplus Ch. ラプラス・ダークネス'
+  },
+  {
+    id: 'UCs9_O1tRPMQTHQ-N_L6FU2g',
+    group: 'ホロライブ',
+    title: 'Lui Ch. 鷹嶺ルイ'
+  },
+  {
+    id: 'UC6eWCld0KwmyHFbAqK3V-Rw',
+    group: 'ホロライブ',
+    title: 'Koyori Ch. 博衣こより'
+  },
+
+  {
+    id: 'UCIBY1ollUsauvVi4hW4cumw',
+    group: 'ホロライブ',
+    title: 'Chloe Ch. 沙花叉クロヱ'
+  },
+  {
+    id: 'UC_vMYWcDjmfdpH6r4TTn1MQ',
+    group: 'ホロライブ',
+    title: 'Iroha Ch. 風真いろは'
+  },
+
+  {
+    id: 'UCJFZiqLMntJufDCHc6bQixg',
+    group: 'ホロライブ',
+    title: 'ホロライブ公式'
+  }
+]
