7b03c3e5e11a386808d64589e6918a66bdb2750e
varmil <fkmks247@gmail.com>
2024-08-04 20:40:27 +0900

    chore

--- backend/apps/closed-api-server/src/closed-api-server.module.ts
+++ backend/apps/closed-api-server/src/closed-api-server.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { CloudSchedulersYoutubeController } from '@presentation/cloud-schedulers/cloud-schedulers-youtube.controller'
+import { CloudSchedulersPresentationModule } from '@presentation/cloud-schedulers/cloud-schedulers.presentation.module'
 import { HealthController } from '@presentation/health/health.controller'
 import { YoutubePresentationModule } from '@presentation/youtube/youtube.presentation.module'
 import { CloudSchedulersYoutubeAppModule } from '@app/cloud-schedulers/youtube/cloud-schedulers-youtube.app.module'
@@ -11,10 +11,11 @@ import { LibAppModule } from '@app/lib/lib.app.module'
     // in only Local, load .env , in other environments, directly embed with Cloud Run
     ConfigModule.forRoot({ ignoreEnvFile: !!process.env.ENV_NAME }),
     LibAppModule,
+    CloudSchedulersPresentationModule,
     YoutubePresentationModule,
     CloudSchedulersYoutubeAppModule
   ],
-  controllers: [HealthController, CloudSchedulersYoutubeController],
+  controllers: [HealthController],
   providers: []
 })
 export class ClosedApiServerModule {}
