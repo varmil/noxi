7d9e7376936c12ec5a282210282c2bc009941c34
varmil <fkmks247@gmail.com>
2024-08-04 21:06:37 +0900

    Impl some logic into hololive-save-aggregations-by-channel

--- backend/apps/hololive-save-aggregations-by-channel/src/hololive-save-aggregations-by-channel.module.ts
+++ backend/apps/hololive-save-aggregations-by-channel/src/hololive-save-aggregations-by-channel.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { HololiveSaveAggregationsByChannelController } from './hololive-save-aggregations-by-channel.controller';
-import { HololiveSaveAggregationsByChannelService } from './hololive-save-aggregations-by-channel.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { LibAppModule } from '@app/lib/lib.app.module'
+import { YoutubeAppModule } from '@app/youtube/youtube.app.module'
 
 @Module({
-  imports: [],
-  controllers: [HololiveSaveAggregationsByChannelController],
-  providers: [HololiveSaveAggregationsByChannelService],
+  imports: [
+    // in only Local, load .env , in other environments, directly embed with Cloud Run
+    ConfigModule.forRoot({ ignoreEnvFile: !!process.env.ENV_NAME }),
+    // NOTE: firebase使わなくなれば不要
+    LibAppModule,
+    YoutubeAppModule
+  ],
+  controllers: [],
+  providers: []
 })
 export class HololiveSaveAggregationsByChannelModule {}
--- backend/apps/hololive-save-aggregations-by-channel/src/main.ts
+++ backend/apps/hololive-save-aggregations-by-channel/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+import { ChannelsScenario } from '@app/youtube/channels/channels.scenario'
+import { YoutubeAppModule } from '@app/youtube/youtube.app.module'
+import { ChannelId } from '@domain/youtube'
 import { HololiveSaveAggregationsByChannelModule } from './hololive-save-aggregations-by-channel.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(HololiveSaveAggregationsByChannelModule)
-  await app.listen(3000)
+  const app = await NestFactory.createApplicationContext(
+    HololiveSaveAggregationsByChannelModule
+  )
+  const channelsScenario = app.select(YoutubeAppModule).get(ChannelsScenario)
+  const videos = await channelsScenario.getVideosInChannel({
+    where: { channelId: new ChannelId('UC-hM6YJuNYVAmUWxeIr9FeA') }
+  })
+  console.log(videos.items.map(e => e.snippet.title))
 }
 bootstrap().catch(reason => console.error(reason))
