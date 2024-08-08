0ef1d7a58e5f96b8139f6d66c92d4936d5518c99
varmil <fkmks247@gmail.com>
2024-08-08 15:15:48 +0900

    Update rechart ^2.13.0-alpha.4

---
+++ web/app/[locale]/(end-user)/hololive/channels/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from 'next'
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server'
+import GlobalBreadcrumb from 'components/GlobalBreadcrumb'
+import Page from 'components/Page'
+import { getChannel } from 'features/youtube/api/getChannel'
+import { ChannelIdDashboard } from 'features/youtube/components/ChannelIdDashboard'
+
+type Props = {
+  params: { locale: string; id: string }
+}
+
+export async function generateMetadata({
+  params: { locale, id }
+}: Props): Promise<Metadata> {
+  const { basicInfo } = await getChannel(id)
+  const tg = await getTranslations({ locale, namespace: 'Global' })
+  const t = await getTranslations({ locale, namespace: 'ChannelsId' })
+
+  return {
+    title: `${basicInfo.title}${t('title')} | ${tg('title')}`,
+    description: `${basicInfo.title}${t('title')}. ${basicInfo.description}`
+  }
+}
+
+export default async function HololiveChannelsIdPage({
+  params: { locale, id }
+}: Props) {
+  // Enable static rendering
+  unstable_setRequestLocale(locale)
+
+  const { basicInfo } = await getChannel(id)
+  const t = await getTranslations('Breadcrumb')
+
+  return (
+    <Page>
+      <GlobalBreadcrumb
+        items={[
+          { href: '/', name: 'YouTube' },
+          { href: '/youtube/charts/channels', name: t('channels') },
+          { href: '#', name: basicInfo.title }
+        ]}
+      />
+      <ChannelIdDashboard id={id} />
+    </Page>
+  )
+}
