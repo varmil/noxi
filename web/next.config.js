d15f754914613cca0221e3da5b6afca958288f45
varmil <fkmks247@gmail.com>
2024-08-10 22:36:45 +0900

    remove public from next cache

--- web/next.config.js
+++ web/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         headers: [
           {
             key: 'CDN-Cache-Control',
-            value: 'public, max-age=10800, s-maxage=10800'
+            value: 'max-age=10800, s-maxage=10800'
           }
         ]
       }
