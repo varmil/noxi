0b7d953e2ab9e4152fd4b542cef21d7d799c68dc
Akihiro Yamamoto <fkmks247@gmail.com>
2024-08-10 20:02:52 +0900

    Add Cache-control for edge cache (#194)

--- web/next.config.js
+++ web/next.config.js
@@ -4,6 +4,19 @@ const withNextIntl = createNextIntlPlugin('./config/i18n/i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=600, s-maxage=3600'
+          }
+        ]
+      }
+    ]
+  }
   // images: {
   //   remotePatterns: [
   //     {
