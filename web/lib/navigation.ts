2c4132490d46efd8a3a260518e2e428b85265954
varmil <fkmks247@gmail.com>
2024-08-10 19:56:14 +0900

    Add Cache-control for edge cache

--- web/lib/navigation.ts
+++ web/lib/navigation.ts
@@ -12,10 +12,11 @@ export const { Link, getPathname, redirect, usePathname, useRouter } =
  * https://stackoverflow.com/questions/64158705/ignore-certain-console-errors-warnings-in-react
  */
 {
+  const warn = console.warn
   const SUPPRESSED_WARNINGS = ['Skipping auto-scroll']
   console.warn = function filterWarnings(msg: string, ...args) {
     if (!SUPPRESSED_WARNINGS.some(entry => msg.startsWith(entry))) {
-      console.warn(msg, ...args)
+      warn(msg, ...args)
     }
   }
 }
