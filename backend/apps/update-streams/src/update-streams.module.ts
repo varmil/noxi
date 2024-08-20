9d73770a97d024a8467e581f993da25cc9ca40e2
varmil <fkmks247@gmail.com>
2024-08-20 20:13:58 +0900

    chore

--- backend/apps/update-streams/src/update-streams.module.ts
+++ backend/apps/update-streams/src/update-streams.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UpdateStreamsController } from './update-streams.controller';
-import { UpdateStreamsService } from './update-streams.service';
+import { Module } from '@nestjs/common'
+import { UpdateStreamsService } from './update-streams.service'
 
 @Module({
   imports: [],
-  controllers: [UpdateStreamsController],
-  providers: [UpdateStreamsService],
+  controllers: [],
+  providers: [UpdateStreamsService]
 })
 export class UpdateStreamsModule {}
