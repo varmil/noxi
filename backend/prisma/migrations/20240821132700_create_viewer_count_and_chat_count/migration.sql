7b94e2a60256b176943a1dcc0b98ac0870de88f3
varmil <fkmks247@gmail.com>
2024-08-21 22:27:33 +0900

    refine schema

--- backend/prisma/migrations/20240821132700_create_viewer_count_and_chat_count/migration.sql
+++ backend/prisma/migrations/20240821132700_create_viewer_count_and_chat_count/migration.sql
@@ -1,20 +1,22 @@
 -- CreateTable
 CREATE TABLE "YoutubeStreamViewerCount" (
+    "id" SERIAL NOT NULL,
     "videoId" TEXT NOT NULL,
     "count" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "YoutubeStreamViewerCount_pkey" PRIMARY KEY ("videoId")
+    CONSTRAINT "YoutubeStreamViewerCount_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "YoutubeStreamChatCount" (
+    "id" SERIAL NOT NULL,
     "videoId" TEXT NOT NULL,
     "all" INTEGER NOT NULL DEFAULT 0,
     "member" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "YoutubeStreamChatCount_pkey" PRIMARY KEY ("videoId")
+    CONSTRAINT "YoutubeStreamChatCount_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
