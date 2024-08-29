bb42a6a8a7c81430ce34851b758a817e20cba173
varmil <fkmks247@gmail.com>
2024-08-29 18:04:06 +0900

    chore

--- backend/libs/domain/hololive/hololive-english.ts
+++ backend/libs/domain/hololive/hololive-english.ts
@@ -1,94 +1,94 @@
 export default [
   {
     id: 'UCL_qhgtOy0dy1Agp8vkySQg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Mori Calliope Ch. hololive-EN'
   },
   {
     id: 'UCHsx4Hqa-1ORjQTh9TYDhww',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Takanashi Kiara Ch. hololive-EN'
   },
   {
     id: 'UCMwGHR0BTZuLsmjY_NT5Pwg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: `Ninomae Ina'nis Ch. hololive-EN`
   },
   {
     id: 'UCoSrY_IQQVpmIRZ9Xf-y93g',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Gawr Gura Ch. hololive-EN'
   },
   {
     id: 'UCyl1z3jo3XHR1riLFKG5UAg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Watson Amelia Ch. hololive-EN'
   },
   {
     id: 'UC8rcEBzJSleTkf_-agPM20g',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'IRyS Ch. hololive-EN'
   },
 
   {
     id: 'UCO_aKKYxn4tvrqPjcTzZ6EQ',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Ceres Fauna Ch. hololive-EN'
   },
   {
     id: 'UCmbs8T6MWqUHP1tIQvSgKrg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Ouro Kronii Ch. hololive-EN'
   },
   {
     id: 'UC3n5uGu18FoCy23ggWWp8tA',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Nanashi Mumei Ch. hololive-EN'
   },
   {
     id: 'UCgmPnx-EEeOrZSg5Tiw7ZRQ',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Hakos Baelz Ch. hololive-EN'
   },
   {
     id: 'UCgnfPPb9JI3e9A4cXHnWbyg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Shiori Novella Ch. hololive-EN'
   },
   {
     id: 'UC9p_lqQ0FEDz327Vgf5JwqA',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Koseki Bijou Ch. hololive-EN'
   },
 
   {
     id: 'UC_sFNM0z0MWm9A6WlKPuMMg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Nerissa Ravencroft Ch. hololive-EN'
   },
   {
     id: 'UCt9H_RpQzhxzlyBxFqrdHqA',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'FUWAMOCO Ch. hololive-EN'
   },
   {
     id: 'UCW5uhrG1eCBYditmhL0Ykjw',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Elizabeth Rose Bloodflame Ch. hololive-EN'
   },
   {
     id: 'UCDHABijvPBnJm7F-KlNME3w',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Gigi Murin Ch. hololive-EN'
   },
   {
     id: 'UCvN5h1ShZtc7nly3pezRayg',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Cecilia Immergreen Ch. hololive-EN'
   },
   {
     id: 'UCl69AEx4MdqMZH7Jtsm7Tig',
-    group: 'hololive',
+    group: 'hololive-english',
     title: 'Raora Panthera Ch. hololive-EN'
   }
 ]
