10407c1bdb5d48b057b56b5c2f05f1b7bfc2388d
varmil <fkmks247@gmail.com>
2024-08-29 18:03:44 +0900

    Create backend/libs/domain/hololive/hololive-english.ts

---
+++ backend/libs/domain/hololive/hololive-english.ts
@@ -0,0 +1,94 @@
+export default [
+  {
+    id: 'UCL_qhgtOy0dy1Agp8vkySQg',
+    group: 'hololive',
+    title: 'Mori Calliope Ch. hololive-EN'
+  },
+  {
+    id: 'UCHsx4Hqa-1ORjQTh9TYDhww',
+    group: 'hololive',
+    title: 'Takanashi Kiara Ch. hololive-EN'
+  },
+  {
+    id: 'UCMwGHR0BTZuLsmjY_NT5Pwg',
+    group: 'hololive',
+    title: `Ninomae Ina'nis Ch. hololive-EN`
+  },
+  {
+    id: 'UCoSrY_IQQVpmIRZ9Xf-y93g',
+    group: 'hololive',
+    title: 'Gawr Gura Ch. hololive-EN'
+  },
+  {
+    id: 'UCyl1z3jo3XHR1riLFKG5UAg',
+    group: 'hololive',
+    title: 'Watson Amelia Ch. hololive-EN'
+  },
+  {
+    id: 'UC8rcEBzJSleTkf_-agPM20g',
+    group: 'hololive',
+    title: 'IRyS Ch. hololive-EN'
+  },
+
+  {
+    id: 'UCO_aKKYxn4tvrqPjcTzZ6EQ',
+    group: 'hololive',
+    title: 'Ceres Fauna Ch. hololive-EN'
+  },
+  {
+    id: 'UCmbs8T6MWqUHP1tIQvSgKrg',
+    group: 'hololive',
+    title: 'Ouro Kronii Ch. hololive-EN'
+  },
+  {
+    id: 'UC3n5uGu18FoCy23ggWWp8tA',
+    group: 'hololive',
+    title: 'Nanashi Mumei Ch. hololive-EN'
+  },
+  {
+    id: 'UCgmPnx-EEeOrZSg5Tiw7ZRQ',
+    group: 'hololive',
+    title: 'Hakos Baelz Ch. hololive-EN'
+  },
+  {
+    id: 'UCgnfPPb9JI3e9A4cXHnWbyg',
+    group: 'hololive',
+    title: 'Shiori Novella Ch. hololive-EN'
+  },
+  {
+    id: 'UC9p_lqQ0FEDz327Vgf5JwqA',
+    group: 'hololive',
+    title: 'Koseki Bijou Ch. hololive-EN'
+  },
+
+  {
+    id: 'UC_sFNM0z0MWm9A6WlKPuMMg',
+    group: 'hololive',
+    title: 'Nerissa Ravencroft Ch. hololive-EN'
+  },
+  {
+    id: 'UCt9H_RpQzhxzlyBxFqrdHqA',
+    group: 'hololive',
+    title: 'FUWAMOCO Ch. hololive-EN'
+  },
+  {
+    id: 'UCW5uhrG1eCBYditmhL0Ykjw',
+    group: 'hololive',
+    title: 'Elizabeth Rose Bloodflame Ch. hololive-EN'
+  },
+  {
+    id: 'UCDHABijvPBnJm7F-KlNME3w',
+    group: 'hololive',
+    title: 'Gigi Murin Ch. hololive-EN'
+  },
+  {
+    id: 'UCvN5h1ShZtc7nly3pezRayg',
+    group: 'hololive',
+    title: 'Cecilia Immergreen Ch. hololive-EN'
+  },
+  {
+    id: 'UCl69AEx4MdqMZH7Jtsm7Tig',
+    group: 'hololive',
+    title: 'Raora Panthera Ch. hololive-EN'
+  }
+]
--- backend/libs/domain/hololive/list.ts
+++ backend/libs/domain/hololive/list.ts
@@ -1,186 +1,186 @@
 export default [
   {
     id: 'UCp6993wxpyDPHUpavwDFqgg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'SoraCh. ときのそらチャンネル'
   },
   {
     id: 'UCDqI2jOz0weumE8s7paEk6g',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Roboco Ch. - ロボ子'
   },
   {
     id: 'UCFTLzh12_nrtzqBPsTCqenA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'アキ・ローゼンタール'
   },
   {
     id: 'UC1CfXB_kRs3C-zaeTG3oGyg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'HAACHAMA Ch 赤井はあと'
   },
 
   {
     id: 'UCdn5BQ06XqgXoAxIhbqw5Rg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'フブキCh。白上フブキ'
   },
   {
     id: 'UCQ0UDLQCjY0rmuxCDE38FGg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Matsuri Channel 夏色まつり'
   },
   {
     id: 'UC1opHUrw8rvnsadT-iGp7Cg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Aqua Ch. 湊あくあ'
   },
   {
     id: 'UCXTpFs_3PqI41qX2d9tL2Rw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Shion Ch. 紫咲シオン'
   },
 
   {
     id: 'UC7fk0CB07ly8oSl0aqKkqFg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Nakiri Ayame Ch. 百鬼あやめ'
   },
   {
     id: 'UC1suqwovbL1kzsoaZgFZLKg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Choco Ch. 癒月ちょこ'
   },
   {
     id: 'UCvzGlP9oQwU--Y0r9id_jnA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Subaru Ch. 大空スバル'
   },
   {
     id: 'UC0TXe_LYZ4scaW2XMyi5_kw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'AZKi Channel'
   },
 
   {
     id: 'UCp-5t9SrOQwXMU7iIjQfARg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Mio Channel 大神ミオ'
   },
   {
     id: 'UC-hM6YJuNYVAmUWxeIr9FeA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Miko Ch. さくらみこ'
   },
   {
     id: 'UCvaTdHTWBGv3MKj3KVqJVCw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Okayu Ch. 猫又おかゆ'
   },
   {
     id: 'UChAnqc_AY5_I3Px5dig3X1Q',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Korone Ch. 戌神ころね'
   },
 
   {
     id: 'UC5CwaMl1eIgY8h02uZw7u8A',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Suisei Channel'
   },
   {
     id: 'UC1DCedRgGHBdm81E1llLhOQ',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Pekora Ch. 兎田ぺこら'
   },
   {
     id: 'UCvInZx9h3jC2JzsIzoOebWg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: '不知火フレア'
   },
   {
     id: 'UCdyqAaZDKHXg4Ahi7VENThQ',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Noel Ch. 白銀ノエル'
   },
 
   {
     id: 'UCCzUftO8KOVkV4wQG1vkUvg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Marine Ch. 宝鐘マリン'
   },
   {
     id: 'UCZlDXzGoo7d44bwdNObFacg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Kanata Ch. 天音かなた'
   },
   {
     id: 'UCqm3BQLlJfvkTsX_hvm0UmA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Watame Ch. 角巻わため'
   },
   {
     id: 'UC1uv2Oq6kNxgATlCiez59hw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Towa Ch. 常闇トワ'
   },
 
   {
     id: 'UCa9Y57gfeY0Zro_noHRVrnw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Luna Ch. 姫森ルーナ'
   },
   {
     id: 'UCFKOVgVbGmX65RxO3EtH3iw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Lamy Ch. 雪花ラミィ'
   },
   {
     id: 'UCAWSyEs_Io8MtpY3m-zqILA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Nene Ch. 桃鈴ねね'
   },
   {
     id: 'UCUKD-uaobj9jiqB-VXt71mA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Botan Ch. 獅白ぼたん'
   },
 
   {
     id: 'UCK9V2B22uJYu3N7eR_BT9QA',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Polka Ch. 尾丸ポルカ'
   },
   {
     id: 'UCENwRMx5Yh42zWpzURebzTw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Laplus Ch. ラプラス・ダークネス'
   },
   {
     id: 'UCs9_O1tRPMQTHQ-N_L6FU2g',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Lui Ch. 鷹嶺ルイ'
   },
   {
     id: 'UC6eWCld0KwmyHFbAqK3V-Rw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Koyori Ch. 博衣こより'
   },
 
   {
     id: 'UCIBY1ollUsauvVi4hW4cumw',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Chloe Ch. 沙花叉クロヱ'
   },
   {
     id: 'UC_vMYWcDjmfdpH6r4TTn1MQ',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'Iroha Ch. 風真いろは'
   },
 
   {
     id: 'UCJFZiqLMntJufDCHc6bQixg',
-    group: 'ホロライブ',
+    group: 'hololive',
     title: 'ホロライブ公式'
   }
 ]
