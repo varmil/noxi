c57f80f053f9dc680ad27d8682ba66436d7ab720
varmil <fkmks247@gmail.com>
2024-08-30 20:34:01 +0900

    refine Q

--- web/features/youtube/highlight-clip/components/HighlightClipGallery.tsx
+++ web/features/youtube/highlight-clip/components/HighlightClipGallery.tsx
@@ -1,4 +1,5 @@
 import { PropsWithoutRef } from 'react'
+import { getLocale, getTranslations } from 'next-intl/server'
 import {
   Card,
   CardContent,
@@ -7,33 +8,55 @@ import {
   CardTitle
 } from '@/components/ui/card'
 import { searchVideos } from 'api/youtube/searchVideos'
+import { GroupString } from 'config/constants/Site'
 import VideoCard from 'features/youtube/video/components/VideoCard'
 import dayjs from 'lib/dayjs'
+import { getGroup } from 'lib/server-only-context/cache'
 
 type Props = { channelId?: string }
 
+const qMap: Record<GroupString, Record<'ja' | 'en', string>> = {
+  hololive: { ja: 'ホロライブ 切り抜き', en: 'hololive clips' },
+  'hololive-english': {
+    ja: 'ホロライブ english 切り抜き',
+    en: 'hololive english clips'
+  },
+  'hololive-indonesia': {
+    ja: 'ホロライブ indonesia 切り抜き',
+    en: 'hololive indonesia clips'
+  }
+}
+
 export async function HighlightClipGallery({}: PropsWithoutRef<Props>) {
+  const groupStr = getGroup()
+  const locale = await getLocale()
   const videos = await searchVideos({
-    q: 'ホロライブ　切り抜き',
+    q: qMap[groupStr][locale],
     limit: 12,
     order: 'relevance',
-    publishedAfter: dayjs().subtract(3, 'days').toDate()
+    publishedAfter: dayjs().subtract(6, 'days').toDate(),
+    language: locale
   })
+
+  const group = (await getTranslations('Global.group'))(`${groupStr}`)
+  const t = await getTranslations('Features.youtube.highlight-clip')
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Latest {videos.length} videos</CardTitle>
-        <CardDescription>
-          The latest {videos.length} videos are displayed.
-        </CardDescription>
+        <CardTitle>{t('title')}</CardTitle>
+        <CardDescription>{t('description', { group })}</CardDescription>
       </CardHeader>
       <CardContent
         className={`grid gap-2 grid-cols-1 sm:grid-cols-3 md:grid-cols-4`}
       >
-        {videos.map(video => {
-          const { id } = video
-          return <VideoCard key={id} {...video} />
-        })}
+        {/* TODO: short, medium - long で分ける */}
+        {videos
+          .sort((a, b) => b.statistics.viewCount - a.statistics.viewCount)
+          .map(video => {
+            const { id } = video
+            return <VideoCard key={id} {...video} />
+          })}
       </CardContent>
     </Card>
   )
