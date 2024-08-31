a2ae09aeea7add3f513bb2889008c92a67901a35
varmil <fkmks247@gmail.com>
2024-08-31 17:43:38 +0900

    WIP Add short card

--- web/features/youtube/highlight-clip/components/HighlightClipGallery.tsx
+++ web/features/youtube/highlight-clip/components/HighlightClipGallery.tsx
@@ -10,6 +10,7 @@ import {
 import { searchVideos } from 'api/youtube/searchVideos'
 import Image from 'components/styles/Image'
 import { GroupString } from 'config/constants/Site'
+import ShortCard from 'features/youtube/short/ShortCard'
 import VideoCard from 'features/youtube/video/components/VideoCard'
 import dayjs from 'lib/dayjs'
 import { getGroup } from 'lib/server-only-context/cache'
@@ -31,13 +32,20 @@ const qMap: Record<GroupString, Record<'ja' | 'en', string>> = {
 export async function HighlightClipGallery({}: PropsWithoutRef<Props>) {
   const groupStr = getGroup()
   const locale = await getLocale()
-  const videos = await searchVideos({
-    q: qMap[groupStr][locale],
-    limit: 12,
-    order: 'relevance',
-    publishedAfter: dayjs().subtract(6, 'days').startOf('day').toDate(),
-    language: locale
-  })
+  const videos = (
+    await searchVideos({
+      q: qMap[groupStr][locale],
+      limit: 50,
+      order: 'relevance',
+      publishedAfter: dayjs().subtract(6, 'days').startOf('day').toDate(),
+      language: locale
+    })
+  ).sort((a, b) => b.statistics.viewCount - a.statistics.viewCount)
+
+  // take first 5
+  const shorts = videos.filter(video => video.isShort).slice(0, 5)
+  // take first 6
+  const notShorts = videos.filter(video => !video.isShort).slice(0, 6)
 
   const group = (await getTranslations('Global.group'))(`${groupStr}`)
   const t = await getTranslations('Features.youtube.highlight-clip')
@@ -61,18 +69,25 @@ export async function HighlightClipGallery({}: PropsWithoutRef<Props>) {
             />
             <span className="font-bold">ショート</span>
           </div>
+
+          <section
+            className={`grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3`}
+          >
+            {shorts.map(video => {
+              const { id } = video
+              return <ShortCard key={id} {...video} />
+            })}
+          </section>
         </section>
 
         {/*  medium - long  */}
         <section
-          className={`grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4`}
+          className={`grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-3`}
         >
-          {videos
-            .sort((a, b) => b.statistics.viewCount - a.statistics.viewCount)
-            .map(video => {
-              const { id } = video
-              return <VideoCard key={id} {...video} />
-            })}
+          {notShorts.map(video => {
+            const { id } = video
+            return <VideoCard key={id} {...video} />
+          })}
         </section>
       </CardContent>
     </Card>
