ee4fd3099a6d9fcd3f1a8be6286c5dbb2591d0f9
varmil <fkmks247@gmail.com>
2024-09-02 20:26:01 +0900

    Modify Sheet

---
+++ web/components/header/HeaderLink.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren, PropsWithoutRef } from 'react'
+import { Link } from 'lib/navigation'
+
+const IconWrapper = ({ children }: PropsWithChildren<{}>) => (
+  <div className="flex justify-center items-center h-8 w-8">{children}</div>
+)
+
+type Props = {
+  name: string
+  icon: JSX.Element
+  href: string
+  active?: boolean
+}
+
+export default function HeaderLink({
+  name,
+  icon,
+  href,
+  active
+}: PropsWithoutRef<Props>) {
+  return (
+    <Link
+      href={href}
+      className={`flex items-center gap-4 px-2.5 ${
+        active ? 'text-foreground' : 'text-muted-foreground'
+      } hover:text-foreground`}
+      prefetch={false}
+    >
+      <IconWrapper>{icon}</IconWrapper>
+      <span className="flex-1">{name}</span>
+    </Link>
+  )
+}
