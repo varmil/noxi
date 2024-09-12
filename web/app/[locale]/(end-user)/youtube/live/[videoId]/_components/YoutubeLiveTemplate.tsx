b4e87c052c046feacb9d4705daa1a7f9c132aa87
varmil <fkmks247@gmail.com>
2024-09-12 17:22:32 +0900

    Refactor

--- web/app/[locale]/(end-user)/youtube/live/[videoId]/_components/YoutubeLiveTemplate.tsx
+++ web/app/[locale]/(end-user)/youtube/live/[videoId]/_components/YoutubeLiveTemplate.tsx
@@ -30,8 +30,92 @@ export default async function YoutubeLiveTemplate({
     group
   } = stream
 
+  return (
+    <div className="grid gap-y-4 lg:gap-x-0 grid-cols-1 lg:grid-cols-[1fr,402px] lg:grid-rows-[auto,1fr]">
+      <section className="space-y-4">
+        <div className="aspect-video overflow-hidden">
+          <iframe
+            src={`https://www.youtube.com/embed/${videoId}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="w-full h-full"
+          ></iframe>
+        </div>
+
+        {/* タイトル、投稿者情報 */}
+        <StreamBasicInfo stream={stream} />
+      </section>
+
+      <section className="space-y-4">
+        {/* Chat */}
+        <section className="h-[calc(100svh-24rem)] lg:h-[calc(100svh-12rem)]">
+          <iframe
+            src={`https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${embed_domain}`}
+            allow=""
+            allowFullScreen
+            className="w-full h-full rounded lg:rounded-none"
+          ></iframe>
+        </section>
+        {/* Related Videos */}
+        <RelatedVideos />
+      </section>
+    </div>
+  )
+}
+
+async function StreamBasicInfo({ stream }: { stream: StreamSchema }) {
+  const {
+    videoId,
+    snippet: { title, channelId },
+    metrics: {
+      peakConcurrentViewers,
+      avgConcurrentViewers,
+      chatMessages,
+      views,
+      likes
+    },
+    group
+  } = stream
   const { basicInfo, statistics } = await getChannel(channelId)
 
+  return (
+    <section className="hidden md:block space-y-4">
+      <h1 className="text-lg sm:text-xl font-bold">{title}</h1>
+      <div className="flex items-center space-x-2">
+        <Avatar className="w-7 h-7 sm:w-11 sm:h-11">
+          <AvatarImage
+            src={basicInfo.thumbnails.medium?.url}
+            alt={basicInfo.title}
+          />
+          <AvatarFallback>{basicInfo.title}</AvatarFallback>
+        </Avatar>
+        <div>
+          <p className="grid grid-cols-5 gap-x-0.5 items-center">
+            <span className="col-span-4 font-semibold">{basicInfo.title}</span>
+            <span className="col-span-1 text-sm text-muted-foreground">
+              <IntlNumberFormat>{statistics.subscriberCount}</IntlNumberFormat>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="flex space-x-2">
+        <Button variant="outline">
+          <ThumbsUp className="mr-2 h-4 w-4" />
+          <IntlNumberFormat>{likes}</IntlNumberFormat>
+        </Button>
+        <Button variant="outline">
+          <MessageSquare className="mr-2 h-4 w-4" />
+          <IntlNumberFormat>{chatMessages}</IntlNumberFormat>
+        </Button>
+        <Button variant="outline">
+          <Share2 className="mr-2 h-4 w-4" /> Share
+        </Button>
+      </div>
+    </section>
+  )
+}
+
+function RelatedVideos() {
   const relatedVideos = [
     {
       id: 1,
@@ -71,88 +155,24 @@ export default async function YoutubeLiveTemplate({
   ]
 
   return (
-    <div className="grid gap-4 md:grid-cols-[2fr,1fr]">
-      <div className="space-y-4">
-        <div className="aspect-video rounded-lg overflow-hidden">
-          <iframe
-            src={`https://www.youtube.com/embed/${videoId}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="w-full h-full"
-          ></iframe>
-        </div>
-
-        {/* タイトル、投稿者情報 */}
-        <section className="hidden md:block space-y-4">
-          <h1 className="text-lg sm:text-xl font-bold">{title}</h1>
-          <div className="flex items-center space-x-2">
-            <Avatar className="w-7 h-7 sm:w-11 sm:h-11">
-              <AvatarImage
-                src={basicInfo.thumbnails.medium?.url}
-                alt={basicInfo.title}
-              />
-              <AvatarFallback>{basicInfo.title}</AvatarFallback>
-            </Avatar>
+    <div className="bg-secondary rounded-lg p-4 space-y-4">
+      <h2 className="text-xl font-semibold">Related Videos</h2>
+      <ScrollArea className="h-[400px]">
+        {relatedVideos.map(video => (
+          <div key={video.id} className="flex space-x-2 mb-4">
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              className="w-40 h-24 object-cover rounded"
+            />
             <div>
-              <p className="grid grid-cols-5 gap-x-0.5 items-center">
-                <span className="col-span-4 font-semibold">
-                  {basicInfo.title}
-                </span>
-                <span className="col-span-1 text-sm text-muted-foreground">
-                  <IntlNumberFormat>
-                    {statistics.subscriberCount}
-                  </IntlNumberFormat>
-                </span>
-              </p>
+              <h3 className="font-semibold">{video.title}</h3>
+              <p className="text-sm text-gray-500">{video.channel}</p>
+              <p className="text-sm text-gray-500">{video.views}</p>
             </div>
           </div>
-          <div className="flex space-x-2">
-            <Button variant="outline">
-              <ThumbsUp className="mr-2 h-4 w-4" />
-              <IntlNumberFormat>{likes}</IntlNumberFormat>
-            </Button>
-            <Button variant="outline">
-              <MessageSquare className="mr-2 h-4 w-4" />
-              <IntlNumberFormat>{chatMessages}</IntlNumberFormat>
-            </Button>
-            <Button variant="outline">
-              <Share2 className="mr-2 h-4 w-4" /> Share
-            </Button>
-          </div>
-        </section>
-      </div>
-
-      <div className="space-y-4">
-        {/* Chat */}
-        <section className="h-[400px] rounded-md border">
-          <iframe
-            src={`https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${embed_domain}`}
-            allow=""
-            allowFullScreen
-            className="w-full h-full rounded-md"
-          ></iframe>
-        </section>
-        {/* Related Videos */}
-        <div className="bg-secondary rounded-lg p-4 space-y-4">
-          <h2 className="text-xl font-semibold">Related Videos</h2>
-          <ScrollArea className="h-[400px]">
-            {relatedVideos.map(video => (
-              <div key={video.id} className="flex space-x-2 mb-4">
-                <img
-                  src={video.thumbnail}
-                  alt={video.title}
-                  className="w-40 h-24 object-cover rounded"
-                />
-                <div>
-                  <h3 className="font-semibold">{video.title}</h3>
-                  <p className="text-sm text-gray-500">{video.channel}</p>
-                  <p className="text-sm text-gray-500">{video.views}</p>
-                </div>
-              </div>
-            ))}
-          </ScrollArea>
-        </div>
-      </div>
+        ))}
+      </ScrollArea>
     </div>
   )
 }
