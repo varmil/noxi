334930dcec42bf7de9c1da62f417f217a4eeb3d1
varmil <fkmks247@gmail.com>
2024-09-13 17:36:48 +0900

    refactor

--- web/app/[locale]/(end-user)/youtube/live/[videoId]/_components/YoutubeLiveTemplate.tsx
+++ web/app/[locale]/(end-user)/youtube/live/[videoId]/_components/YoutubeLiveTemplate.tsx
@@ -2,11 +2,10 @@ import { PropsWithoutRef } from 'react'
 import { StreamSchema } from 'apis/youtube/schema/streamSchema'
 import LgContainer from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/layouts/LgContainer'
 import PadSection from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/layouts/PadSection'
+import EmbedLiveChat from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/ui/EmbedLiveChat'
+import EmbedStream from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/ui/EmbedStream'
 import RelatedVideos from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/ui/RelatedVideos'
 import StreamBasicInfo from 'app/[locale]/(end-user)/youtube/live/[videoId]/_components/ui/StreamBasicInfo'
-
-const embed_domain =
-  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL || 'localhost'
 
 type Props = {
   stream: StreamSchema
@@ -27,23 +26,12 @@ export default async function YoutubeLiveTemplate({
     group
   } = stream
 
-  const EMBED_QUERY = new URLSearchParams({
-    autoplay: '1',
-    mute: '1',
-    modestbranding: '1'
-  }).toString()
-
   return (
     <div className="grid lg:gap-x-0 grid-cols-1 lg:grid-cols-[minmax(650px,100%),minmax(320px,1fr)]">
       <LgContainer className="space-y-4">
         {/* Stream */}
         <div className="flex order-1 aspect-video w-full max-h-[94svh] justify-center items-center bg-black">
-          <iframe
-            src={`https://www.youtube.com/embed/${videoId}?${EMBED_QUERY}`}
-            allow="accelerometer; autoplay; encrypted-media; picture-in-picture; web-share"
-            allowFullScreen
-            className="aspect-video h-full max-h-[94svh]"
-          ></iframe>
+          <EmbedStream videoId={videoId} className="h-full max-h-[94svh]" />
         </div>
 
         {/* タイトル、投稿者情報 */}
@@ -56,12 +44,7 @@ export default async function YoutubeLiveTemplate({
         {/* Chat */}
         <PadSection right className="order-2">
           <section className="min-h-80 h-[calc(100svh-24rem)] lg:h-[calc(100svh-10rem)]">
-            <iframe
-              src={`https://www.youtube.com/live_chat?v=${videoId}&embed_domain=${embed_domain}`}
-              allow=""
-              allowFullScreen
-              className="w-full h-full rounded-lg lg:rounded-none"
-            ></iframe>
+            <EmbedLiveChat videoId={videoId} />
           </section>
         </PadSection>
 
