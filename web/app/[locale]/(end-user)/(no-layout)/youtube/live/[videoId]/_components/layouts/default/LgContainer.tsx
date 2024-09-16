01eae2394028d40b1e40e8d2ef7a499708f9446d
varmil <fkmks247@gmail.com>
2024-09-16 19:28:55 +0900

    Impl Open Chat State

--- web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/layouts/default/LgContainer.tsx
+++ web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/layouts/default/LgContainer.tsx
@@ -1,13 +1,37 @@
-import { PropsWithChildren } from 'react'
+'use client'
+
+import { useGlobalOpenLiveChat } from 'app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_hooks/youtubeLiveStates'
+
+type Props = {
+  className?: string
+  children: React.ReactNode
+}
 
 /** LG以上のブレークポイントでシンプルな２カラムを表現するために使用する */
-export default function LgContainer({
-  className,
-  children
-}: PropsWithChildren<{ className?: string }>) {
+function LgContainer({ className, children }: Props) {
   return (
-    <section className={`contents lg:block ${className ?? ''}`}>
+    <section className={`@container contents lg:block ${className ?? ''}`}>
       {children}
     </section>
   )
+}
+
+/** lg以上で表示する左コンテナ */
+export function LeftContainer(props: Props) {
+  return (
+    <LgContainer
+      {...props}
+      className={`space-y-4 lg:flex-1 lg:grid ${props.className ?? ''}`}
+    />
+  )
+}
+
+/** lg以上で表示するチャットリストコンテナ */
+export function RightContainer(props: Omit<Props, 'className'>) {
+  const { isOpenLiveChat } = useGlobalOpenLiveChat()
+  if (isOpenLiveChat) {
+    return <LgContainer {...props} className="lg:w-[350px]" />
+  } else {
+    return <LgContainer {...props} className="hidden" />
+  }
 }
--- web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/ui/mode/DefaultModeTemplate.tsx
+++ web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/ui/mode/DefaultModeTemplate.tsx
@@ -1,7 +1,10 @@
 import { PropsWithoutRef } from 'react'
 import { getStream } from 'apis/youtube/getStream'
 import OpenChatButton from 'app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/ui/button/OpenChatButton'
-import LgContainer from '../../layouts/default/LgContainer'
+import {
+  LeftContainer,
+  RightContainer
+} from '../../layouts/default/LgContainer'
 import PadSection from '../../layouts/default/PadSection'
 import MaximizeButton from '../button/MaximizeButton'
 import EmbedLiveChat from '../stream/EmbedLiveChat'
@@ -32,33 +35,39 @@ export default async function DefaultModeTemplate({
 
   return (
     <div className="grid grid-cols-1 lg:flex lg:gap-x-0">
-      <LgContainer className="space-y-4 lg:flex-1 lg:grid lg:grid-cols-3">
+      <LeftContainer className="lg:grid-cols-3">
         {/* Stream */}
         <section className="order-1 aspect-video w-full bg-black lg:col-span-full">
           <EmbedStream videoId={videoId} className="w-full h-full" />
         </section>
 
         {/* タイトル、投稿者情報 */}
-        <PadSection left className="order-3 space-y-4 lg:col-span-2">
+        <PadSection
+          left
+          className="order-3 space-y-4 @xs:col-span-full @4xl:col-span-2"
+        >
           <MaximizeButton />
           <StreamBasicInfo stream={stream} />
         </PadSection>
 
         {/* Open ChatList & Related Videos */}
-        <PadSection right className="order-4 space-y-4 lg:col-span-1">
+        <PadSection
+          right
+          className="order-4 space-y-4 @xs:col-span-full @4xl:col-span-1"
+        >
           <OpenChatButton />
           <RelatedVideos />
         </PadSection>
-      </LgContainer>
+      </LeftContainer>
 
-      <LgContainer className="lg:w-80">
+      <RightContainer>
         {/* Chat */}
         <PadSection className="lg:px-0 order-2">
           <section className="min-h-80 h-[calc(100vh-25rem)] lg:h-[calc(100vh-9.5rem)]">
             <EmbedLiveChat videoId={videoId} />
           </section>
         </PadSection>
-      </LgContainer>
+      </RightContainer>
     </div>
   )
 }
