b3e78dc3cd80b65a3b52e5d4919cfbb443a24626
varmil <fkmks247@gmail.com>
2024-09-16 19:49:16 +0900

    Hide OpenChatButton when already shown

--- web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/ui/button/OpenChatButton.tsx
+++ web/app/[locale]/(end-user)/(no-layout)/youtube/live/[videoId]/_components/ui/button/OpenChatButton.tsx
@@ -3,8 +3,13 @@
 import { Button } from '@/components/ui/button'
 import { useGlobalOpenLiveChat } from '../../../_hooks/youtubeLiveStates'
 
+/** すでに表示している場合は何も表示しない */
 export default function OpenChatButton() {
-  const { setOpenLiveChat } = useGlobalOpenLiveChat()
+  const { isOpenLiveChat, setOpenLiveChat } = useGlobalOpenLiveChat()
+
+  if (isOpenLiveChat) {
+    return null
+  }
 
   return (
     <Button
