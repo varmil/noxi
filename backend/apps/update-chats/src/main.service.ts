8b85f898c287aaec4e275643f471b25dd69693b0
varmil <fkmks247@gmail.com>
2024-09-23 19:47:05 +0900

    chore

--- backend/apps/update-chats/src/main.service.ts
+++ backend/apps/update-chats/src/main.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 
 @Injectable()
 export class MainService {
+  private readonly logger = new Logger(MainService.name)
+
   async getHello(): Promise<string> {
     // sleep 20 seconds
     await new Promise(resolve => setTimeout(resolve, 20000))
-    console.log('Hello World! (after 20 seconds)')
+    this.logger.log('Hello World! (after 20 seconds)')
     return 'Hello World!'
   }
 }
