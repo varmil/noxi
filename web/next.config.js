4b80fb1668653ecd1945bc40cacc6a1ad8002135
varmil <fkmks247@gmail.com>
2024-10-03 17:37:50 +0900

    disble CDN cache

--- web/next.config.js
+++ web/next.config.js
@@ -5,27 +5,27 @@ const withNextIntl = createNextIntlPlugin('./config/i18n/i18n.ts')
 /**
  * exclude paths which may include query params
  */
-const cacheSources = [
-  '/:locale',
-  '/:locale/terms-of-use-and-privacy-policy',
-  '/:locale/:path*/charts/chanels',
-  '/:locale/:path*/chanels/:channelId'
-]
-const CacheHeaders = cacheSources.map(source => ({
-  source,
-  headers: [
-    {
-      key: 'CDN-Cache-Control',
-      value: 'public, max-age=10800, s-maxage=10800'
-    }
-  ]
-}))
+// const cacheSources = [
+//   '/:locale',
+//   '/:locale/terms-of-use-and-privacy-policy',
+//   '/:locale/:path*/charts/chanels',
+//   '/:locale/:path*/chanels/:channelId'
+// ]
+// const CacheHeaders = cacheSources.map(source => ({
+//   source,
+//   headers: [
+//     {
+//       key: 'CDN-Cache-Control',
+//       value: 'public, max-age=10800, s-maxage=10800'
+//     }
+//   ]
+// }))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return CacheHeaders
-  },
+  // async headers() {
+  //   return CacheHeaders
+  // },
 
   logging: {
     fetches: {
