fbad44d5ffb929ea137e5eb8abd92efc299d5fcc
varmil <fkmks247@gmail.com>
2024-10-05 20:32:42 +0900

    chore

--- web/features/youtube/video/components/VideoInChannelGallery.tsx
+++ web/features/youtube/video/components/VideoInChannelGallery.tsx
@@ -1,4 +1,5 @@
 import { PropsWithoutRef } from 'react'
+import { getTranslations } from 'next-intl/server'
 import {
   Card,
   CardContent,
@@ -15,6 +16,7 @@ type Props = { channelId: string }
 export async function VideoInChannelGallery({
   channelId
 }: PropsWithoutRef<Props>) {
+  const t = await getTranslations('Features.youtube.video')
   const videos = (await getVideosInChannel({ channelId, limit: 50 })).filter(
     video => !video.liveStreamingDetails
   )
@@ -29,12 +31,18 @@ export async function VideoInChannelGallery({
           Displays non-live streaming videos. Includes videos and shorts.
         </CardDescription>
       </CardHeader>
-      <CardContent className={`grid gap-x-4 gap-y-4 ${gridClassName}`}>
-        {videos.map(video => {
-          const { id } = video
-          return <VideoCard key={id} {...video} />
-        })}
-      </CardContent>
+      {videos.length === 0 ? (
+        <CardContent>
+          <p className="text-muted-foreground">{t('noVideos')}</p>
+        </CardContent>
+      ) : (
+        <CardContent className={`grid gap-x-4 gap-y-4 ${gridClassName}`}>
+          {videos.map(video => {
+            const { id } = video
+            return <VideoCard key={id} {...video} />
+          })}
+        </CardContent>
+      )}
     </Card>
   )
 }
