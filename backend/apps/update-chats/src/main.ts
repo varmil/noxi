377beff8aef3c53478a5e75d99e06dd7741ef4b2
Akihiro Yamamoto <fkmks247@gmail.com>
2024-10-21 19:06:46 +0900

    bugfix (#596)

--- backend/apps/update-chats/src/main.ts
+++ backend/apps/update-chats/src/main.ts
@@ -12,7 +12,7 @@ const INTERVAL_MS = 15000
  * 1時間+1分の間、INTERVAL_MS間隔で実行する
  * 1分はバッファで敢えて次の実行と被せる。Cloud Schedulerは1時間間隔。
  */
-const EXECUTE_COUNT = 3660 / INTERVAL_MS
+const EXECUTE_COUNT = (3660 * 1000) / INTERVAL_MS
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(MainModule)
