53951cada32a1755b8c1bd02dfe7301b0ac0aaec
varmil <fkmks247@gmail.com>
2024-10-28 16:49:46 +0900

    chore

--- backend/libs/domain/index.ts
+++ backend/libs/domain/index.ts
@@ -1,7 +1,6 @@
 export * from './chat-bundle-queue'
 export * from './country'
 export * from './group'
-export * from './group/channele
 export * from './stream'
 export * from './supers'
 export * from './time-period'
