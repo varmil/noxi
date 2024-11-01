10a8bd697e0f865be3e715eda4df9be121fda975
varmil <fkmks247@gmail.com>
2024-11-01 20:32:00 +0900

    delete Client file

--- web/app/[locale]/(end-user)/(default)/_components/ui/tabs/ChannelCommentTabs.tsx
+++ web/app/[locale]/(end-user)/(default)/_components/ui/tabs/ChannelCommentTabs.tsx
@@ -1,26 +1,46 @@
-import { ChannelCommentTabsOnClient } from 'app/[locale]/(end-user)/(default)/_components/ui/tabs/ChannelCommentTabsOnClient'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import SuperChatGallery from 'features/supers/chat/components/SuperChatGallery'
 import YoutubeCommentGallery from 'features/youtube/comment/YoutubeCommentGallery'
 
 export function ChannelCommentTabs({ channelId }: { channelId: string }) {
   return (
-    <ChannelCommentTabsOnClient
-      superChat={
-        <SuperChatGallery
-          channelId={channelId}
-          createdAfter={new Date(new Date().getTime() - 168 * 60 * 60 * 1000)}
-          limit={30}
-          showStreamLink
-        />
-      }
-      comments={
-        <YoutubeCommentGallery
-          channelId={channelId}
-          order="time"
-          limit={30}
-          showStreamLink
-        />
-      }
-    />
+    <Tabs defaultValue={'superChat'} className="w-full">
+      <TabsList className="w-full mb-4">
+        <TabsTrigger className="flex-1" value="superChat">
+          Super Chat
+        </TabsTrigger>
+        <TabsTrigger className="flex-1" value="comments">
+          Comments
+        </TabsTrigger>
+      </TabsList>
+
+      <ScrollArea className="h-[493px] rounded-md border p-4">
+        <TabsContent
+          forceMount
+          value="superChat"
+          className={'data-[state=inactive]:content-visibility-hidden'}
+        >
+          <SuperChatGallery
+            channelId={channelId}
+            createdAfter={new Date(new Date().getTime() - 168 * 60 * 60 * 1000)}
+            limit={30}
+            showStreamLink
+          />
+        </TabsContent>
+        <TabsContent
+          forceMount
+          value="comments"
+          className={'data-[state=inactive]:content-visibility-hidden'}
+        >
+          <YoutubeCommentGallery
+            channelId={channelId}
+            order="time"
+            limit={30}
+            showStreamLink
+          />
+        </TabsContent>
+      </ScrollArea>
+    </Tabs>
   )
 }
