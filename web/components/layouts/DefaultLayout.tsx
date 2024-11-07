5672f42c6081dcf89775ee84f4b60f24ebd609a9
varmil <fkmks247@gmail.com>
2024-11-07 18:00:13 +0900

    :bug: z-index

--- web/components/layouts/DefaultLayout.tsx
+++ web/components/layouts/DefaultLayout.tsx
@@ -5,9 +5,11 @@ import { CookieAgreeBanner } from 'components/banners/CookieAgreeBanner'
 export default async function DefaultLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <Aside />
-      <section className="sm:pb-4 sm:pl-14 bg-muted/40">{children}</section>
-      <CookieAgreeBanner />
+      <Aside className="z-10" />
+      <section className="relative z-0 sm:pb-4 sm:pl-14 bg-muted/40">
+        {children}
+      </section>
+      <CookieAgreeBanner className="z-20" />
     </>
   )
 }
