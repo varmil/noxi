cf75a40eab743df640c05bbf1fc00f7f398b922e
varmil <fkmks247@gmail.com>
2024-11-08 15:25:28 +0900

    Set min-w-7 for CountryFlag

--- web/features/stream-ranking/components/table/StreamRankingTable.tsx
+++ web/features/stream-ranking/components/table/StreamRankingTable.tsx
@@ -48,7 +48,7 @@ export default async function StreamRankingTable({ compact }: Props) {
           return (
             <TableRow key={videoId}>
               {/* Flag */}
-              <TableCell className="p-0 sm:p-2 justify-items-center w-8">
+              <TableCell className="p-0 sm:p-2 justify-items-center min-w-7 w-8">
                 <CountryFlag countryCode={channel.peakX?.country} size={20} />
               </TableCell>
 
