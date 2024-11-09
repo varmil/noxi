7c1ea2d60a5534e7f7087291e12ef702e0277b09
varmil <fkmks247@gmail.com>
2024-11-09 16:46:10 +0900

    fold Channel Data (WIP)

--- web/app/[locale]/(end-user)/(default)/[group]/channels/[id]/_components/ui/channel-data/ChannelData.tsx
+++ web/app/[locale]/(end-user)/(default)/[group]/channels/[id]/_components/ui/channel-data/ChannelData.tsx
@@ -1,5 +1,13 @@
 'use client'
 
+import React from 'react'
+import { ChevronDown } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger
+} from '@/components/ui/collapsible'
 import { ChannelSchema } from 'apis/youtube/schema/channelSchema'
 import StatsJoinedCard from 'features/youtube-stats/components/simple-card/StatsJoinedCard'
 import StatsSubscribersCard from 'features/youtube-stats/components/simple-card/StatsSubscribersCard'
@@ -8,14 +16,29 @@ import StatsViewsCard from 'features/youtube-stats/components/simple-card/StatsV
 
 export default function ChannelData({ channel }: { channel: ChannelSchema }) {
   const { basicInfo, statistics } = channel
+  const [isOpen, setIsOpen] = React.useState(false)
+  const xsClasses = isOpen ? 'block' : 'hidden'
+
   return (
     <>
       <StatsSubscribersCard count={statistics?.subscriberCount ?? 0} />
       <StatsViewsCard count={statistics?.viewCount ?? 0} />
-      <StatsVideosCard count={statistics?.videoCount ?? 0} />
-      <StatsJoinedCard
-        date={new Date(basicInfo?.publishedAt).toDateString() ?? 'N/A'}
+      <StatsVideosCard
+        className={`${xsClasses} lg:block`}
+        count={statistics?.videoCount ?? 0}
       />
+      <StatsJoinedCard
+        className={`${xsClasses} lg:block`}
+        date={new Date(basicInfo?.publishedAt).toISOString() ?? 'N/A'}
+      />
+
+      <div className="lg:hidden">
+        {isOpen ? null : (
+          <Button variant="outline" onClick={() => setIsOpen(true)}>
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
     </>
   )
 }
