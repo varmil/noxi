bd9dec4ec12cdc7465e290d74c3c723fab0303b2
varmil <fkmks247@gmail.com>
2024-11-10 20:19:18 +0900

    refactor

--- web/app/[locale]/(end-user)/(default)/page.tsx
+++ web/app/[locale]/(end-user)/(default)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import { getTranslations, setRequestLocale } from 'next-intl/server'
+import { getOgUrl } from 'utils/og-url'
 import { Page } from 'components/page'
 import { IndexTemplate } from './_components/IndexTemplate'
 
@@ -13,28 +14,10 @@ export async function generateMetadata({
   const tg = await getTranslations({ locale, namespace: 'Global' })
   const t = await getTranslations({ locale, namespace: 'Page.index' })
 
-  const {
-    NEXT_PUBLIC_VERCEL_ENV,
-    NEXT_PUBLIC_VERCEL_BRANCH_URL,
-    NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-  } = process.env
-
-  let URL = ''
-  switch (NEXT_PUBLIC_VERCEL_ENV) {
-    case 'production':
-      URL = `https://${NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ?? ''}`
-      break
-    case 'preview':
-      URL = `https://${NEXT_PUBLIC_VERCEL_BRANCH_URL ?? ''}`
-      break
-    default:
-      URL = 'http://localhost:3000'
-  }
-
   return {
     title: `${t('metadata.title')} - ${tg('title')}`,
     description: `${t('metadata.description')}`,
-    openGraph: { images: [{ url: `${URL}/api/og/daily-ranking` }] }
+    openGraph: { images: [{ url: getOgUrl('/daily-ranking') }] }
   }
 }
 
