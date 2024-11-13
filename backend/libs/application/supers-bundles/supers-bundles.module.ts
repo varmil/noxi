f8f776993b0b1a4b324f47940ddafce9f90ee000
varmil <fkmks247@gmail.com>
2024-11-13 19:45:41 +0900

    Impl Application layer

---
+++ backend/libs/application/supers-bundles/supers-bundles.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { SupersBundlesService } from '@app/supers-bundles/supers-bundles.service'
+import { SupersBundleInfraModule } from '@infra/supers-bundle/supers-bundle.infra.module'
+
+@Module({
+  imports: [SupersBundleInfraModule],
+  providers: [SupersBundlesService],
+  exports: [SupersBundleInfraModule, SupersBundlesService]
+})
+export class SupersBundlesModule {}
