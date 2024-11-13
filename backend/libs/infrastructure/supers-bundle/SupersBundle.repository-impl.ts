31860ed1a5d8fb833cbe7f0fea0121bf8c97bc86
varmil <fkmks247@gmail.com>
2024-11-13 19:41:40 +0900

    Impl infra layer

---
+++ backend/libs/infrastructure/supers-bundle/SupersBundle.repository-impl.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@nestjs/common'
+import BigNumber from 'bignumber.js'
+import { AmountMicros, Group } from '@domain'
+import {
+  SupersBundleRepository,
+  SupersBundles,
+  SupersBundle,
+  SupersCount
+} from '@domain/supers-bundle'
+import {
+  ActualEndTime,
+  ActualStartTime,
+  ChannelId,
+  VideoId
+} from '@domain/youtube'
+import { PrismaInfraService } from '@infra/service/prisma/prisma.infra.service'
+
+@Injectable()
+export class SupersBundleRepositoryImpl implements SupersBundleRepository {
+  constructor(private readonly prismaInfraService: PrismaInfraService) {}
+
+  async findAll({
+    where,
+    orderBy,
+    limit,
+    offset
+  }: Parameters<SupersBundleRepository['findAll']>[0]) {
+    const whereQuery = where
+      ? {
+          videoId: where.videoId?.get(),
+          channelId: where.channelId?.get(),
+          group: where.group?.get(),
+          actualEndTime: {
+            gte: where.actualEndTime?.gte
+          }
+        }
+      : undefined
+
+    const rows =
+      await this.prismaInfraService.youtubeStreamSupersBundle.findMany({
+        where: whereQuery,
+        orderBy,
+        take: limit,
+        skip: offset
+      })
+
+    return new SupersBundles(
+      rows.map(
+        row =>
+          new SupersBundle({
+            videoId: new VideoId(row.videoId),
+            channelId: new ChannelId(row.channelId),
+            amountMicros: new AmountMicros(
+              BigNumber(row.amountMicros.toString())
+            ),
+            count: new SupersCount(row.count),
+            actualStartTime: new ActualStartTime(row.actualStartTime),
+            actualEndTime: new ActualEndTime(row.actualEndTime),
+            group: new Group(row.group)
+          })
+      )
+    )
+  }
+
+  async save({ data }: Parameters<SupersBundleRepository['save']>[0]) {
+    const {
+      videoId,
+      channelId,
+      amountMicros,
+      count,
+      actualStartTime,
+      actualEndTime,
+      group
+    } = data
+
+    const update = {
+      channelId: channelId.get(),
+      amountMicros: amountMicros.toBigInt(),
+      count: count.get(),
+      actualStartTime: actualStartTime.get(),
+      actualEndTime: actualEndTime.get(),
+      group: group.get()
+    }
+
+    await this.prismaInfraService.youtubeStreamSupersBundle.upsert({
+      where: { videoId: videoId.get() },
+      update,
+      create: { videoId: videoId.get(), ...update }
+    })
+  }
+}
