e3a62c4e954fe1b137cb34d704b4bac5c37c3e97
varmil <fkmks247@gmail.com>
2024-11-14 19:35:48 +0900

    Add date parameter

--- web/app/[locale]/(end-user)/(default)/page.tsx
+++ web/app/[locale]/(end-user)/(default)/page.tsx
@@ -1,23 +1,36 @@
 import { Metadata } from 'next'
 import { getTranslations, setRequestLocale } from 'next-intl/server'
 import { Page } from 'components/page'
+import dayjs from 'lib/dayjs'
 import { getOgUrl } from 'utils/og-url'
 import { IndexTemplate } from './_components/IndexTemplate'
 
 type Props = {
   params: { locale: string }
+  searchParams: { date?: string }
 }
 
 export async function generateMetadata({
-  params: { locale }
+  params: { locale },
+  searchParams: { date }
 }: Props): Promise<Metadata> {
   const tg = await getTranslations({ locale, namespace: 'Global' })
   const t = await getTranslations({ locale, namespace: 'Page.index' })
 
+  const searchParams = new URLSearchParams({
+    ...(date && { date: dayjs(date).toISOString() })
+  })
+
   return {
     title: `${t('metadata.title')} - ${tg('title')}`,
     description: `${t('metadata.description')}`,
-    openGraph: { images: [{ url: getOgUrl('/daily-ranking') }] }
+    openGraph: {
+      images: [
+        {
+          url: getOgUrl(`/daily-ranking?${searchParams.toString()}`)
+        }
+      ]
+    }
   }
 }
 
