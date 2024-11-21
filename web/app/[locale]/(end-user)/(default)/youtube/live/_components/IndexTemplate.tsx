cd9511b49eac96e1694b0e5c255b81bf78a51a45
varmil <fkmks247@gmail.com>
2024-11-21 17:57:13 +0900

    Add web/components/ranking/filter/RankingFilterGallery.tsx

--- web/app/[locale]/(end-user)/(default)/youtube/live/_components/IndexTemplate.tsx
+++ web/app/[locale]/(end-user)/(default)/youtube/live/_components/IndexTemplate.tsx
@@ -1,12 +1,14 @@
 import { PropsWithoutRef } from 'react'
 import { PageSMPX } from 'components/page'
+import RankingFilterGallery from 'components/ranking/filter/RankingFilterGallery'
 import StreamRankingGallery from 'features/stream-ranking/components/gallery/StreamRankingGallery'
 
 type Props = {}
 
 export default function IndexTemplate({}: PropsWithoutRef<Props>) {
   return (
-    <section className={`max-w-6xl ${PageSMPX} mx-auto mt-8`}>
+    <section className={`max-w-6xl ${PageSMPX} mx-auto mt-8 space-y-8`}>
+      <RankingFilterGallery />
       <StreamRankingGallery />
     </section>
   )
