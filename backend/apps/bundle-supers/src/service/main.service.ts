1a407ccdc1610c299c43e1d61b8fa84a06f405eb
varmil <fkmks247@gmail.com>
2024-11-22 19:49:31 +0900

    Bundle Realtime

--- backend/apps/bundle-supers/src/service/main.service.ts
+++ backend/apps/bundle-supers/src/service/main.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@nestjs/common'
 import { SuperChatsService } from '@app/super-chats/super-chats.service'
 import { SuperStickersService } from '@app/super-stickers/super-stickers.service'
-import { Streams } from '@domain/stream'
+import { Streams, StreamStatus, StreamStatuses } from '@domain/stream'
 import { SupersCount } from '@domain/supers-bundle'
 import { VideoId } from '@domain/youtube'
+import { StreamsService } from '@app/streams/streams.service'
+import { SupersBundleQueuesService } from '@app/supers-bundle-queues/supers-bundle-queues.service'
 
 @Injectable()
 export class MainService {
   constructor(
+    private readonly streamsService: StreamsService,
+    private readonly supersBundleQueuesService: SupersBundleQueuesService,
     private readonly superChatsService: SuperChatsService,
     private readonly superStickersService: SuperStickersService
   ) {}
+
+  /** live中（リアルタイム） */
+  async fetchLives() {
+    return await this.streamsService.findAll({
+      where: {
+        status: new StreamStatuses([new StreamStatus('live')])
+      },
+      limit: 1000
+    })
+  }
+
+  /**  queue */
+  async fetchQueues() {
+    return await this.supersBundleQueuesService.findAll({
+      limit: 100
+    })
+  }
 
   async calculateTotalInJPY(videoId: VideoId) {
     const { amountMicros: chatTotalInJPY, count: chatCount } =
@@ -51,10 +72,8 @@ export class MainService {
       snippet: { channelId },
       group
     } = stream
-    if (!actualStartTime || !actualEndTime) {
-      throw new Error(
-        `actualStartTime or actualEndTime not found for ${videoId.get()}`
-      )
+    if (!actualStartTime) {
+      throw new Error(`actualStartTime not found for ${videoId.get()}`)
     }
     return {
       actualStartTime,
