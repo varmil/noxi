480bf55a5c8a16b158f9a984487bfa84d8074757
Akihiro Yamamoto <fkmks247@gmail.com>
2024-11-24 00:27:56 +0900

    Handle Unarchived stream pubsub callback (#793)

--- backend/apps/closed-api-server/src/presentation/youtube/pubsubhubbub/pubsubhubbub.presentation.module.ts
+++ backend/apps/closed-api-server/src/presentation/youtube/pubsubhubbub/pubsubhubbub.presentation.module.ts
@@ -5,9 +5,19 @@ import { GroupsAppModule } from '@app/groups/groups.app.module'
 import { StreamsModule } from '@app/streams/streams.module'
 import { YoutubeAppModule } from '@app/youtube/youtube.app.module'
 import { PubsubhubbubScenario } from './pubsubhubbub.scenario'
+import { LibAppModule } from '@app/lib/lib.app.module'
+import { ChatBundleQueuesModule } from '@app/chat-bundle-queues/chat-bundle-queues.module'
+import { SupersBundleQueuesModule } from '@app/supers-bundle-queues/supers-bundle-queues.module'
 
 @Module({
-  imports: [GroupsAppModule, StreamsModule, YoutubeAppModule],
+  imports: [
+    LibAppModule,
+    ChatBundleQueuesModule,
+    SupersBundleQueuesModule,
+    GroupsAppModule,
+    StreamsModule,
+    YoutubeAppModule
+  ],
   controllers: [PubsubhubbubController],
   providers: [PubsubhubbubScenario, CryptoService]
 })
