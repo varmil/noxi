ea8862e73daea45c12d9efca706315bbbd1ea480
varmil <fkmks247@gmail.com>
2024-11-27 23:44:42 +0900

    refactor

--- backend/apps/groups/summarize-channels/src/main.module.ts
+++ backend/apps/groups/summarize-channels/src/main.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { GroupsAppModule } from '@app/groups/groups.app.module'
 import { LibAppModule } from '@app/lib/lib.app.module'
 import { SupersBundlesAppModule } from '@app/supers-bundles/supers-bundles.module'
 import { SupersSummariesAppModule } from '@app/supers-summaries/supers-summaries.module'
 import { YoutubeAppModule } from '@app/youtube/youtube.app.module'
 import { MainScenario } from './scenario/main.scenario'
+import { CreateSupersSummariesService } from './service/create-supers-summaries.service'
 
 @Module({
   imports: [
     // in only Local, load .env , in other environments, directly embed with Cloud Run
     ConfigModule.forRoot({ ignoreEnvFile: !!process.env.ENV_NAME }),
     LibAppModule,
-    GroupsAppModule,
     SupersBundlesAppModule,
     SupersSummariesAppModule,
     YoutubeAppModule
   ],
   controllers: [],
-  providers: [MainScenario]
+  providers: [MainScenario, CreateSupersSummariesService]
 })
 export class MainModule {}
--- backend/apps/groups/summarize-channels/src/scenario/main.scenario.ts
+++ backend/apps/groups/summarize-channels/src/scenario/main.scenario.ts
@@ -1,15 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common'
-import BigNumber from 'bignumber.js'
-import { AmountMicros } from '@domain'
-import { GroupsService } from '@app/groups/groups.service'
-import { PromiseService } from '@app/lib/promise-service'
-import { SupersBundlesService } from '@app/supers-bundles/supers-bundles.service'
-import { SupersSummariesService } from '@app/supers-summaries/supers-summaries.service'
 import { ChannelsService } from '@app/youtube/channels/channels.service'
-import { Now } from '@domain/lib'
-import { AmountMicrosSum } from '@domain/supers-bundle'
-import { SupersSummary } from '@domain/supers-summary'
-import { ChannelId, Channels } from '@domain/youtube/channel'
+import { CreateSupersSummariesService } from '../service/create-supers-summaries.service'
 
 @Injectable()
 export class MainScenario {
@@ -17,11 +8,8 @@ export class MainScenario {
   private readonly logger = new Logger(MainScenario.name)
 
   constructor(
-    private readonly promiseService: PromiseService,
-    private readonly channelsService: ChannelsService,
-    private readonly groupsService: GroupsService,
-    private readonly supersBundlesService: SupersBundlesService,
-    private readonly supersSummariesService: SupersSummariesService
+    private readonly createSupersSummariesService: CreateSupersSummariesService,
+    private readonly channelsService: ChannelsService
   ) {}
 
   async execute(): Promise<void> {
@@ -38,64 +26,11 @@ export class MainScenario {
         offset += this.CHUNK_SIZE
 
         console.time(`processChunk: ${index(offset)}`)
-        await this.processChunk(channels)
+        await this.createSupersSummariesService.execute(channels)
         console.timeEnd(`processChunk: ${index(offset)}`)
       } catch (error) {
         this.logger.error(`Error in chunk: ${index(offset)}:`, error)
       }
     }
-  }
-
-  private async processChunk(channels: Channels): Promise<void> {
-    const now = new Now()
-    const channelIds = channels.ids()
-    const where = (d: Date) => ({ channelIds, actualEndTime: { gte: d } })
-
-    const last7Days = await this.supersBundlesService.sum({
-      where: where(now.xDaysAgo(7))
-    })
-    const last30Days = await this.supersBundlesService.sum({
-      where: where(now.xDaysAgo(30))
-    })
-    const last90Days = await this.supersBundlesService.sum({
-      where: where(now.xDaysAgo(90))
-    })
-    const last1Year = await this.supersBundlesService.sum({
-      where: where(now.xYearsAgo(1))
-    })
-    const thisWeek = await this.supersBundlesService.sum({
-      where: where(now.startOfWeek())
-    })
-    const thisMonth = await this.supersBundlesService.sum({
-      where: where(now.startOfMonth())
-    })
-    const thisYear = await this.supersBundlesService.sum({
-      where: where(now.startOfyear())
-    })
-
-    const getAmountMicros = (data: AmountMicrosSum[], channelId: ChannelId) => {
-      return (
-        data.find(c => c.channelId.equals(channelId))?.amountMicros ??
-        new AmountMicros(BigNumber(0))
-      )
-    }
-
-    const promises = channelIds.map(async channelId => {
-      await this.supersSummariesService.create({
-        data: new SupersSummary({
-          channelId,
-          last7Days: getAmountMicros(last7Days, channelId),
-          last30Days: getAmountMicros(last30Days, channelId),
-          last90Days: getAmountMicros(last90Days, channelId),
-          last1Year: getAmountMicros(last1Year, channelId),
-          thisWeek: getAmountMicros(thisWeek, channelId),
-          thisMonth: getAmountMicros(thisMonth, channelId),
-          thisYear: getAmountMicros(thisYear, channelId),
-          createdAt: now.get()
-        })
-      })
-    })
-
-    await this.promiseService.allSettled(promises)
   }
 }
