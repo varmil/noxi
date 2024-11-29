2fd78dc79d26b1b1ded5192e8e3a821b587d0857
varmil <fkmks247@gmail.com>
2024-11-29 20:19:16 +0900

    Add 44 talents

--- backend/libs/domain/group/channel/nijisanji.ts
+++ backend/libs/domain/group/channel/nijisanji.ts
@@ -408,10 +408,240 @@ const List: GroupChannel[] = [
     title: 'ソフィア・ヴァレンタイン / Sophia Valentine【にじさんじ】',
     ...DefaultProps
   },
-
+  {
+    id: new ChannelId('UC5dJFf4m-mEcoyJRfhBljoA'),
+    title: 'セラフ・ダズルガーデン / Seraph Dazzlegarden',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCUP8TmlO7NNra88AMqGU_vQ'),
+    title: '小清水 透 / Koshimizu Toru【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCpJtk0myFr5WnyfsmnInP-w'),
+    title: 'Hana Macchia Ch.【NIJISANJI・にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UC6WFKwYptsxVue56Lx218vg'),
+    title: '小柳ロウ / Koyanagi Rou【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCe22Bcwd_GCpTjLxn83zl7A'),
+    title: '先斗寧 / Ponto Nei 【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCfQVs_KuXeNAlGa3fb8rlnQ'),
+    title: '桜凛月',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCt0clH12Xk1-Ej5PXKGfdPA'),
+    title: '♥️♠️物述有栖♦️♣️',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UC2OacIzd2UxGHRGhdHl1Rhw'),
+    title: '早瀬 走 / Hayase Sou【にじさんじ所属】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCWz0CSYCxf4MhRKPDm220AQ'),
+    title: '神田笑一 / Kanda Shoichi【 にじさんじ 】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCC7rRD6P7RQcx0hKv9RQP4w'),
+    title: '風楽奏斗 / Fura Kanato',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCqXxS-9x9Ha_UiH6hG4kh5Q'),
+    title: '緋八マナ / Hibachi Mana【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCgZ0pH7j6c9z-pkOG3PYw1Q'),
+    title: '星導ショウ / Hoshirube Sho【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCwrjITPwG4q71HzihV2C7Nw'),
+    title: 'フミ/にじさんじ',
+    ...DefaultProps
+  },
   {
     id: new ChannelId('UC7_MFM9b8hp5kuTSpa8WyOQ'),
     title: '栞葉るり / Shioriha Ruri【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCtLfA_qUqCJtjXJM2ZR_keg'),
+    title: '石神のぞみ / Ishigami Nozomi【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCmeyo5pRj_6PXG-CsGUuWWg'),
+    title: '黒井しば【にじさんじの犬】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCllKI7VjyANuS1RXatizfLQ'),
+    title: '山神 カルタ / Karuta Yamagami',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCb6ObE-XGCctO3WrjRZC-cw'),
+    title: 'ルイス・キャミー',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UC8SJPcLpIM3x8RdBJFfiluw'),
+    title: '魁星 / Kaisei【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UC_GCs6GARLxEHxy1w40d6VQ'),
+    title: '家長むぎ【にじさんじ所属】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCRcLAVTbmx2-iNcXSsupdNA'),
+    title: '来栖 夏芽-kurusu natsume-【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCtHY-tP0dyykhTRMmnfPs_g'),
+    title: '海妹四葉 / Umise Yotsuha 【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCy8P3o5XlMpJGQY4WugzdNA'),
+    title: '佐伯イッテツ / Saiki Ittetsu【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCWRPqA0ehhWV4Hnp27PJCkQ'),
+    title: '獅子堂 あかり / Shishido Akari【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCpNH2Zk2gw3JBjWAKSyZcQQ'),
+    title: 'エリー・コニファー / Eli Conifer【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCg63a3lk6PNeWhVvMRM_mrQ'),
+    title: '小野町 春香 / Onomachi Haruka 【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCu-rV2gPtJ-CsGxe71z_BrQ'),
+    title: '五十嵐梨花 / Igarashi Rika 【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCl1oLKcAq93p-pwKfDGhiYQ'),
+    title: 'えま★おうがすと / Emma★August【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCeGendL8CO5RkffB6IFwHow'),
+    title: 'セフィナ / Seffyna【にじさんじ】',
+    ...DefaultProps,
+    country: new CountryCode('KR')
+  },
+  {
+    id: new ChannelId('UCyXBNgCulibV9pRm3ZKpmoQ'),
+    title: '榊ネス / Sakaki Ness【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCHX7YpFG8rVwhsHCx34xt7w'),
+    title: '雪城眞尋/Yukishiro Mahiro【にじさんじ所属】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCBi8YaVyZpiKWN3_Z0dCTfQ'),
+    title: '赤羽葉子ちゃんねる',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UChdY64fJb14Nfnbs8EGdQig'),
+    title: '叢雲カゲツ / Murakumo Kagetsu【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCz89MGFBrAqwJ5xMr5weSuA'),
+    title: '伊波ライ / Inami Rai【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCambvP8yxNDot4FzQc9cgiw'),
+    title: '宇佐美リト / Usami Rito【にじさんじ】',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UC_82HBGtvwN1hcGeOGHzUBQ'),
+    title: '空星きらめ/Sorahoshi Kirame【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCmUjjW5zF1MMOhYUwwwQv9Q'),
+    title: '宇志海いちご',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UC0WwEfE-jOM2rzjpdfhTzZA'),
+    title: '愛園 愛美/Aizono Manami',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCiA-trSZfB0i92V_-dyDqBw'),
+    title: '倉持めると / Kuramochi Meruto【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UClrQ7xhRBxS_v_-WuudGKmA'),
+    title: '鏑木ろこ / Kaburaki Roco【にじさんじ】',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCtAvQ5U0aXyKwm2i4GqFgJg'),
+    title: '春崎エアル',
+    ...DefaultProps,
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCHK5wkevfaGrPr7j3g56Jmw'),
+    title: '瀬戸 美夜子 - Miyako Seto',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCUtKkGKef8BYMs3h-3zQm9A'),
+    title: '민수하 /Suhaスハ【NIJISANJI】',
+    ...DefaultProps,
+    country: new CountryCode('KR'),
+    gender: Gender.Male
+  },
+  {
+    id: new ChannelId('UCRWOdwLRsenx2jLaiCAIU4A'),
+    title: '雨森小夜',
     ...DefaultProps
   }
 ]
