b10773055ae66f25df084f86bfbb3a858504f785
varmil <fkmks247@gmail.com>
2024-12-01 18:55:59 +0900

    refine StreamRankingTableTitle.tsx

--- web/features/channels-ranking/components/table/ChannelsRankingTableTitle.tsx
+++ web/features/channels-ranking/components/table/ChannelsRankingTableTitle.tsx
@@ -1,7 +1,6 @@
 import { PropsWithChildren } from 'react'
 import { ChevronRight, TvMinimalPlayIcon } from 'lucide-react'
 import { useTranslations } from 'next-intl'
-import Image from 'components/styles/Image'
 import {
   ChannelsRankingPeriod,
   ChannelsRankingDimension
@@ -23,22 +22,20 @@ export default function ChannelsRankingTableTitle({
   const feat = useTranslations('Features.channelsRanking')
   return (
     <section className={`${className || ''}`}>
-      <div className={`flex flex-row gap-x-1 items-center`}>
-        <h1 className="flex text-sm sm:text-base items-center">
-          <TvMinimalPlayIcon className="w-6 h-6 mr-3" />
-          <div className="flex gap-x-1 sm:gap-x-2 items-center line-clamp-1">
-            <span className="flex-1 break-anywhere line-clamp-1">
-              {breadcrumb('channelsRanking')}
-            </span>
-            <ChevronRight className="relative w-3 h-3 top-[1px]" />
-            <span className="line-clamp-1">
-              {feat(`ranking.dimension.${dimension}`, {
-                period: global(`period.${period}`)
-              })}
-            </span>
-          </div>
-        </h1>
-      </div>
+      <h1 className="flex text-sm sm:text-base items-center">
+        <TvMinimalPlayIcon className="w-6 h-6 mr-3" />
+        <div className="flex gap-x-1 sm:gap-x-2 items-center line-clamp-1">
+          <span className="flex-1 break-anywhere line-clamp-1">
+            {breadcrumb('channelsRanking')}
+          </span>
+          <ChevronRight className="relative w-3 h-3 top-[1px]" />
+          <span className="line-clamp-1">
+            {feat(`ranking.dimension.${dimension}`, {
+              period: global(`period.${period}`)
+            })}
+          </span>
+        </div>
+      </h1>
     </section>
   )
 }
