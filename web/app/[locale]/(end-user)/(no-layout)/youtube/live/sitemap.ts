2300b7d7c4b2125a01d12804692aa0891d752922
varmil <fkmks247@gmail.com>
2024-12-03 18:14:38 +0900

    refine

--- web/app/[locale]/(end-user)/(no-layout)/youtube/live/sitemap.ts
+++ web/app/[locale]/(end-user)/(no-layout)/youtube/live/sitemap.ts
@@ -2,28 +2,29 @@ import { getStreams } from 'apis/youtube/getStreams'
 import { getEntry } from 'config/sitemap/getEntry'
 import type { MetadataRoute } from 'next'
 
-const LIMIT = 10000
+const LIMIT = 2000
 
+// Streamsの総数 / LIMIT = 総ページ数
 export async function generateSitemaps() {
-  // Fetch the total number of products and calculate the number of sitemaps needed
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
+  return [...Array(10)].map((_, i) => ({ id: i }))
 }
 
+// Google's limit is 50,000 URLs per sitemap
 export default async function sitemap({
   id
 }: {
   id: number
 }): Promise<MetadataRoute.Sitemap> {
-  // Google's limit is 50,000 URLs per sitemap
   const streams = await getStreams({
     orderBy: [{ field: 'videoId', order: 'asc' }],
     limit: LIMIT,
     offset: id * LIMIT
   })
+  console.log('streams', streams.length)
   return streams.map(stream =>
     getEntry({
-      pathname: `/youtube/live/${stream.videoId}`,
-      lastModified: stream.updatedAt
+      pathname: `/youtube/live/${stream.videoId}`
+      // lastModified: stream.updatedAt
     })
   )
 }
