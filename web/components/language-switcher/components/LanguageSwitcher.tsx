ad209af0221b70e2faf37ed86bfc25263a892834
varmil <fkmks247@gmail.com>
2024-12-20 13:55:35 +0900

    refactor

--- web/components/language-switcher/components/LanguageSwitcher.tsx
+++ web/components/language-switcher/components/LanguageSwitcher.tsx
@@ -11,9 +11,26 @@ import { useLocale } from 'next-intl'
 import { Button } from '@/components/ui/button'
 
 export default function LanguageSwitcher() {
+  return (
+    <div className="flex items-center gap-2 text-sm font-medium">
+      <LanguageButton locale="ja">JP</LanguageButton>
+      <span className="text-muted-foreground">|</span>
+      <LanguageButton locale="en">EN</LanguageButton>
+    </div>
+  )
+}
+
+const LanguageButton = ({
+  children,
+  locale
+}: {
+  children: React.ReactNode
+  locale: 'ja' | 'en'
+}) => {
+  const currentLocale = useLocale()
   const pathname = usePathname()
   const searchParams = useSearchParams()
-  const locale = useLocale()
+  const isActive = currentLocale === locale
 
   const createPathForLanguage = (newLocale: string) => {
     // パスの最初の言語部分を新しい言語に置き換え
@@ -25,30 +42,14 @@ export default function LanguageSwitcher() {
   }
 
   return (
-    <div className="flex items-center gap-2 text-sm font-medium">
-      <Button
-        asChild
-        variant="ghost"
-        className={`h-auto px-1 py-0.5 font-light ${
-          locale === 'ja'
-            ? 'font-semibold border-b-2 border-primary rounded-none'
-            : ''
-        }`}
-      >
-        <Link href={createPathForLanguage('ja')}>JP</Link>
-      </Button>
-      <span className="text-muted-foreground">|</span>
-      <Button
-        asChild
-        variant="ghost"
-        className={`h-auto px-1 py-0.5 font-light ${
-          locale === 'en'
-            ? 'font-semibold border-b-2 border-primary rounded-none'
-            : ''
-        }`}
-      >
-        <Link href={createPathForLanguage('en')}>EN</Link>
-      </Button>
-    </div>
+    <Button
+      asChild
+      variant="ghost"
+      className={`h-auto px-1 py-0.5 font-light ${
+        isActive ? 'font-semibold border-b-2 border-primary rounded-none' : ''
+      }`}
+    >
+      <Link href={createPathForLanguage(locale)}>{children}</Link>
+    </Button>
   )
 }
