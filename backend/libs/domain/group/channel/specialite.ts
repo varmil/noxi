5d54d33d54a601e5a0c8206ac75e3bce432011fe
varmil <fkmks247@gmail.com>
2024-12-23 18:21:57 +0900

    finalize talents

--- backend/libs/domain/group/channel/specialite.ts
+++ backend/libs/domain/group/channel/specialite.ts
@@ -14,44 +14,125 @@ const DefaultProps = {
 
 const List: GroupChannel[] = [
   {
-    id: new ChannelId('UCi9O6GC9DvnrXqQfgXrNaYg'),
-    title: '空奏イト',
+    id: new ChannelId('UCej9K_bcaQdvDh49RkHYsUw'),
+    title: '小鳥谷なの',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UCjN-1QSJtD6RdRGXTf9L1Sw'),
-    title: '雪白キャル',
+    id: new ChannelId('UC_bxkbJzxRKssMf52wVbssw'),
+    title: '尾幌こま',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UCAoKkBp0Qj0xW0NEOp7G4hw'),
-    title: '渚沢シチ',
+    id: new ChannelId('UC9Vq5c7FKEHLstvmSAsogXQ'),
+    title: '藍坂しう',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UCc_r_D9M7BGFpwXZnFaxEww'),
-    title: '成海ミャオ',
+    id: new ChannelId('UCHt4F3--Hq0sNXP6DEdlSmg'),
+    title: 'Miki Hitsugi',
+    ...DefaultProps,
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCPm_htRQjzhgFikcO_LTvTA'),
+    title: 'Utahime Mochizuki',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCo9wBz0FkJwiLeagB8tWXCg'),
+    title: 'Victoria Valerie',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCchGYP5t5Vz48UVxNOifk-Q'),
+    title: 'Miu Akumiya',
+    ...DefaultProps,
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UC5UPG3fV3zzuq-JLPw-XdWQ'),
+    title: '本阿弥あずさ',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UCoqYYjzSW9HZSaJHzSLmApA'),
-    title: '天吹サン',
+    id: new ChannelId('UCm4QiJHdsgz9PshJkGMSk5w'),
+    title: 'ミーニャ・スコット',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UCVAyRbZsu5lrv2_xvgliB2Q'),
-    title: '小鈴りあん',
+    id: new ChannelId('UCwBmSNUOWnowsju5YG7dT6g'),
+    title: '領国つかさ',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UC6BCNzMKS01UZT90lrwA1bw'),
-    title: '星乃りむ',
+    id: new ChannelId('UCVvI7-8tzOVC6HUdgQrqSog'),
+    title: '夜炎よる',
     ...DefaultProps
   },
   {
-    id: new ChannelId('UC1AqiQjSbkS_b--haYNkLrw'),
-    title: '琴宮いおり',
+    id: new ChannelId('UCkeECSuB4aRbUyQrgR2SJOQ'),
+    title: '智念せいら',
     ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCez6PoUm-tuIzHLXiXDGKWg'),
+    title: '星屑ぷらね',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCsLdP5LylKUGVQV2lki6kcw'),
+    title: '茶々良かな',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCFaGKEDWe9_jOmWMsyatxSw'),
+    title: '終末むくろ',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCK_lMxHpJFGCGz3gXJgaoQw'),
+    title: '雪芽るみ',
+    ...DefaultProps
+  },
+  {
+    id: new ChannelId('UCNKf15eqoAhxJ6CalqgVSCA'),
+    title: 'Elyza . EVE',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCjzH42Z3aiRxY6SBP_MeihA'),
+    title: 'Bluebell . EVE',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCckvfYHqNxF1y9FdXHlE7Dw'),
+    title: 'Mischief . EVE',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCWD4WUf0UQX4-wY1ouALGuA'),
+    title: 'Hysteria . EVE',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
+  },
+  {
+    id: new ChannelId('UCdbMJDaiIT4viraMae5v4_g'),
+    title: 'Azeria . EVE',
+    ...DefaultProps,
+    country: new CountryCode('US'),
+    defaultLangage: new LanguageTag('en')
   }
 ]
 
