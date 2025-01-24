46544f1846436943222df619ae82e564929327fb
varmil <fkmks247@gmail.com>
2025-01-24 17:10:57 +0900

    :bug: Fix bundle-supers, bundle-chats errors

--- backend/libs/application/supers-bundle-queues/supers-bundle-queues.module.ts
+++ backend/libs/application/supers-bundle-queues/supers-bundle-queues.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
 import { SupersBundleQueuesService } from '@app/supers-bundle-queues/supers-bundle-queues.service'
+import { StreamInfraModule } from '@infra/stream/stream.infra.module'
 import { SupersBundleQueueInfraModule } from '@infra/supers-bundle-queue/supers-bundle-queue.infra.module'
 
 @Module({
-  imports: [SupersBundleQueueInfraModule],
+  imports: [SupersBundleQueueInfraModule, StreamInfraModule],
   providers: [SupersBundleQueuesService],
-  exports: [SupersBundleQueueInfraModule, SupersBundleQueuesService]
+  exports: [
+    SupersBundleQueueInfraModule,
+    StreamInfraModule,
+    SupersBundleQueuesService
+  ]
 })
 export class SupersBundleQueuesModule {}
