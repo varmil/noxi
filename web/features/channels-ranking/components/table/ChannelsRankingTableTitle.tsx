9274894ef2634459502b1c7fcaca7bb45607aec9
varmil <fkmks247@gmail.com>
2025-03-01 14:06:00 +0900

    Add title, aria-label for ranking H1 tags

--- web/features/channels-ranking/components/table/ChannelsRankingTableTitle.tsx
+++ web/features/channels-ranking/components/table/ChannelsRankingTableTitle.tsx
@@ -26,22 +26,25 @@ export default function ChannelsRankingTableTitle({
 }: Props) {
   const global = useTranslations('Global')
   const feat = useTranslations('Features.channelsRanking')
+  const title = feat(`ranking.dimension.${dimension}`, {
+    period: global(`period.${period}`),
+    group: group ? global(`group.${group}`) : 'VTuber',
+    gender: gender ? global(`gender.${gender}`) : ''
+  })
+    .replace(/\s+/g, ' ')
+    .trim()
   return (
     <section className={`flex text-sm sm:text-base ${className || ''}`}>
       <ChartNoAxesColumnIncreasing className="w-6 h-6 mr-3" />
 
       <div className="flex flex-col gap-y-2 sm:w-full sm:flex-row sm:justify-between">
-        <h1 className="flex items-center font-bold">
+        <h1
+          title={title}
+          className="flex items-center font-bold"
+          aria-label={title}
+        >
           <div className="flex gap-x-1 sm:gap-x-2 items-center line-clamp-1">
-            <span className="line-clamp-1">
-              {feat(`ranking.dimension.${dimension}`, {
-                period: global(`period.${period}`),
-                group: group ? global(`group.${group}`) : 'VTuber',
-                gender: gender ? global(`gender.${gender}`) : ''
-              })
-                .replace(/\s+/g, ' ')
-                .trim()}
-            </span>
+            <span className="line-clamp-1">{title}</span>
           </div>
         </h1>
 
