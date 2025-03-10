7cbddcaf851d1868803bd502762d1e6e3c795007
varmil <fkmks247@gmail.com>
2025-03-10 21:02:28 +0900

    Add SupersBundle Earnings for Stream.tsx

--- web/features/group/ended/components/EndedStreams.tsx
+++ web/features/group/ended/components/EndedStreams.tsx
@@ -1,17 +1,22 @@
 import { getStatistics } from 'apis/youtube/data-api/getStatistics'
 import { getChannels } from 'apis/youtube/getChannels'
+import { getSupersBundles } from 'apis/youtube/getSupersBundles'
 import { StreamsSchema } from 'apis/youtube/schema/streamSchema'
 import Stream from 'features/group/stream/components/Stream'
 
 type Props = { streams: StreamsSchema }
 
 export default async function EndedStreams({ streams }: Props) {
-  const [channels, statisticsList] = await Promise.all([
+  const [channels, statisticsList, supersBundles] = await Promise.all([
     getChannels({
       ids: streams.map(stream => stream.snippet.channelId),
       limit: streams.length
     }),
-    getStatistics({ videoIds: streams.map(stream => stream.videoId) })
+    getStatistics({ videoIds: streams.map(stream => stream.videoId) }),
+    getSupersBundles({
+      videoIds: streams.map(stream => stream.videoId),
+      limit: streams.length
+    })
   ])
 
   return (
@@ -29,16 +34,21 @@ export default async function EndedStreams({ streams }: Props) {
           // ここをコメントアウト外すと削除された配信は非表示になる
           // if (!video) return null
 
-          return { stream, channel, video }
+          const bundle = supersBundles.find(
+            bundle => bundle.videoId === stream.videoId
+          )
+
+          return { stream, channel, video, bundle }
         })
         .filter(item => item !== null)
-        .map(({ stream, channel, video }) => {
+        .map(({ stream, channel, video, bundle }) => {
           return (
             <Stream
               key={stream.videoId}
               stream={stream}
               channel={channel}
               statistics={video?.statistics}
+              supersBundle={bundle}
             />
           )
         })}
