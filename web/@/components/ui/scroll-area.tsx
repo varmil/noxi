94af54e9666b78c5eb42d9709c761523d2e1aae4
varmil <fkmks247@gmail.com>
2025-03-12 22:58:53 +0900

    Make darker when dark mode, add Text Saturation when light mode

--- web/@/components/ui/scroll-area.tsx
+++ web/@/components/ui/scroll-area.tsx
@@ -49,7 +49,7 @@ function ScrollBar({
     >
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-border/75 relative flex-1 rounded-full"
+        className="bg-border-variant/75 relative flex-1 rounded-full"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
