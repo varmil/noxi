94c6516b3b57d35593f66336c2a1908940727330
varmil <fkmks247@gmail.com>
2025-03-13 20:26:47 +0900

    chore

--- web/components/ranking/filter/column/Column.tsx
+++ web/components/ranking/filter/column/Column.tsx
@@ -13,13 +13,13 @@ export const ColumnHeader = ({
   showSearch
 }: PropsWithChildren<{ showSearch?: boolean }>) => (
   <div className="flex items-center justify-between mb-4 text-muted-foreground">
-    <h3 className="font-medium">{children}</h3>
+    <h3 className="tracking-tight font-medium">{children}</h3>
     {showSearch ? <Search className="size-4" /> : null}
   </div>
 )
 
 export const ColumnContent = ({ children }: PropsWithChildren) => (
-  <ScrollArea className="h-[168px] sm:h-[185px]">
+  <ScrollArea className="h-[168px] sm:h-[184px]">
     <div className="flex flex-col gap-y-1">{children}</div>
   </ScrollArea>
 )
