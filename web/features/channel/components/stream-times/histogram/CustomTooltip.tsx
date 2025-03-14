0686fd0abacd0d4df55035a1f503e30b27696262
Akihiro Yamamoto <fkmks247@gmail.com>
2025-03-14 22:24:54 +0900

    Chore UI (#1483)

--- web/features/channel/components/stream-times/histogram/CustomTooltip.tsx
+++ web/features/channel/components/stream-times/histogram/CustomTooltip.tsx
@@ -14,16 +14,16 @@ export default function CustomTooltip({ payload }: Props) {
   if (!data) return null
 
   return (
-    <div className="rounded-lg border bg-background p-2 shadow-xs">
+    <div className="rounded-lg border bg-popover text-popover-foreground p-2 shadow-xs text-xs sm:text-sm">
       <div className="grid grid-cols-2 gap-2">
-        <div className="flex flex-col">
-          <span className="text-[0.70rem] uppercase text-muted-foreground">
+        <div className="flex flex-col gap-1">
+          <span className="uppercase text-muted-foreground">
             {t('timeSlot')}
           </span>
           <span className="font-bold text-muted-foreground">{data.text}</span>
         </div>
-        <div className="flex flex-col">
-          <span className="text-[0.70rem] uppercase text-muted-foreground">
+        <div className="flex flex-col gap-1">
+          <span className="uppercase text-muted-foreground">
             {t('streamFrequency')}
           </span>
           <span className="font-bold">{data.count}</span>
