22acc23c26e1a3918c9672a6de6857504c904d3d
varmil <fkmks247@gmail.com>
2025-03-15 17:01:44 +0900

    Smaller Column, 25 --> 20 Channels Ranking Pagination

--- web/components/ranking/filter/column/Column.tsx
+++ web/components/ranking/filter/column/Column.tsx
@@ -3,8 +3,8 @@ import { Search } from 'lucide-react'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
 export const Column = ({ children }: PropsWithChildren) => (
-  <div className="flex-1 pt-4 pb-0 min-w-[144px]">
-    <ScrollArea className="h-[192px] sm:h-[212px] px-4 overflow-hidden">
+  <div className="flex-1 py-0 min-w-[136px]">
+    <ScrollArea className="h-[200px] sm:h-[216px] px-4 overflow-hidden">
       {children}
     </ScrollArea>
   </div>
@@ -14,7 +14,7 @@ export const ColumnHeader = ({
   children,
   showSearch
 }: PropsWithChildren<{ showSearch?: boolean }>) => (
-  <div className="flex items-center justify-between mb-4">
+  <div className="flex items-center justify-between my-4">
     <h3 className="text-sm">{children}</h3>
     {showSearch ? <Search className="size-4" /> : null}
   </div>
