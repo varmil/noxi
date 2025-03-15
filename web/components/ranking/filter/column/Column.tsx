10c661b21f56ceb354f2b44370a8f4012fcd9993
Akihiro Yamamoto <fkmks247@gmail.com>
2025-03-15 17:03:43 +0900

    Refine Column (#1487)
    * Refine Column
    
    * Smaller Column, 25 --> 20 Channels Ranking Pagination

--- web/components/ranking/filter/column/Column.tsx
+++ web/components/ranking/filter/column/Column.tsx
@@ -3,8 +3,10 @@ import { Search } from 'lucide-react'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
 export const Column = ({ children }: PropsWithChildren) => (
-  <div className="flex-1 pt-4 px-4 pb-0 min-w-[151px] overflow-hidden">
-    {children}
+  <div className="flex-1 py-0 min-w-[136px]">
+    <ScrollArea className="h-[200px] sm:h-[216px] px-4 overflow-hidden">
+      {children}
+    </ScrollArea>
   </div>
 )
 
@@ -12,14 +14,12 @@ export const ColumnHeader = ({
   children,
   showSearch
 }: PropsWithChildren<{ showSearch?: boolean }>) => (
-  <div className="flex items-center justify-between mb-4 text-muted-foreground">
-    <h3 className="tracking-tight font-medium">{children}</h3>
+  <div className="flex items-center justify-between my-4">
+    <h3 className="text-sm">{children}</h3>
     {showSearch ? <Search className="size-4" /> : null}
   </div>
 )
 
 export const ColumnContent = ({ children }: PropsWithChildren) => (
-  <ScrollArea className="h-[164px] sm:h-[184px]">
-    <div className="flex flex-col gap-0.5">{children}</div>
-  </ScrollArea>
+  <div className="flex flex-col gap-0.5">{children}</div>
 )
