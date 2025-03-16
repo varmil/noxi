0e4240cd24aacbab2eaa1d85f580ef05bc81f83e
varmil <fkmks247@gmail.com>
2025-03-16 14:39:19 +0900

    Custom web/@/components/ui/scroll-area.tsx

--- web/@/components/ui/scroll-area.tsx
+++ web/@/components/ui/scroll-area.tsx
@@ -28,6 +28,11 @@ function ScrollArea({
   )
 }
 
+/**
+ * @Custom
+ * w-2.5 --> w-2, h-2.5 --> h-2
+ * rounded-full --> rounded-xs
+ */
 function ScrollBar({
   className,
   orientation = 'vertical',
@@ -40,16 +45,16 @@ function ScrollBar({
       className={cn(
         'flex touch-none p-px transition-colors select-none',
         orientation === 'vertical' &&
-          'h-full w-2.5 border-l border-l-transparent',
+          'h-full w-2 border-l border-l-transparent',
         orientation === 'horizontal' &&
-          'h-2.5 flex-col border-t border-t-transparent',
+          'h-2 flex-col border-t border-t-transparent',
         className
       )}
       {...props}
     >
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-border relative flex-1 rounded-full"
+        className="bg-border relative flex-1 rounded-xs"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   )
