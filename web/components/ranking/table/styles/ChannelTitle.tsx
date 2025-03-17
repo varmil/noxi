52a68bee57e0b2b2c6369d602b8344bb74e09593
varmil <fkmks247@gmail.com>
2025-03-17 17:49:57 +0900

    Refine PC Ranking row UI

--- web/components/ranking/table/styles/ChannelTitle.tsx
+++ web/components/ranking/table/styles/ChannelTitle.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function ChannelTitle({ channel, className }: Props) {
   return (
     <div className={`flex items-center hover:underline ${className || ''}`}>
-      <span className="tracking-tighter @lg:tracking-normal line-clamp-1 break-all">
+      <span className="sm:font-medium tracking-tighter @lg:tracking-normal line-clamp-1 break-all">
         {channel.basicInfo.title}
       </span>
     </div>
