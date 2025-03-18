5b5977d9d819fa2b843a70fc1d4650131153e30a
varmil <fkmks247@gmail.com>
2025-03-18 22:27:41 +0900

    Add gap-y for ChannelTitle

--- web/components/ranking/table/styles/ChannelTitle.tsx
+++ web/components/ranking/table/styles/ChannelTitle.tsx
@@ -12,7 +12,9 @@ export default function ChannelTitle({ channel, group, className }: Props) {
   const global = useTranslations('Global')
   return (
     <div
-      className={`flex flex-col gap-y-0 items-start group ${className || ''}`}
+      className={`flex flex-col gap-y-1.5 @lg:gap-y-2 items-start group ${
+        className || ''
+      }`}
     >
       <span className="font-medium tracking-tight @lg:tracking-normal line-clamp-1 break-all group-hover:underline">
         {channel.basicInfo.title}
