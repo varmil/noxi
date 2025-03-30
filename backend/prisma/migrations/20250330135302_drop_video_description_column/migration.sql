ec6cfa9a87df0e377995cc1bbffc633c6e042d95
varmil <fkmks247@gmail.com>
2025-03-30 22:56:45 +0900

    Drop YoutubeStream.description from DB

---
+++ backend/prisma/migrations/20250330135302_drop_video_description_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `YoutubeStream` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "YoutubeStream" DROP COLUMN "description";
