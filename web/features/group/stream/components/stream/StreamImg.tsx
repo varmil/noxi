8f2262a85e5c4833a680df69d9ad27de6b48b066
Akihiro Yamamoto <fkmks247@gmail.com>
2025-04-05 23:08:14 +0900

    Refine StreamImg.tsx (#1676)

--- web/features/group/stream/components/stream/StreamImg.tsx
+++ web/features/group/stream/components/stream/StreamImg.tsx
@@ -12,7 +12,7 @@ export default function StreamImg({
   } = stream
 
   return (
-    <div className="relative w-36 aspect-video rounded overflow-hidden">
+    <div className="relative w-46 aspect-video rounded-tl-xl overflow-hidden">
       <VideoThumbnail size="default" title={title} thumbnails={thumbnails} />
       {children}
     </div>
