16ba2382d49a1f23f87888bb614080ab8d97370f
varmil <fkmks247@gmail.com>
2025-04-15 17:51:22 +0900

    Upgrade Prisma to 6.6.0

--- backend/apps/update-chats/src/main.ts
+++ backend/apps/update-chats/src/main.ts
@@ -7,12 +7,12 @@ import { MainScenario } from './scenario/main.scenario'
 /**
  * 実行間隔
  */
-const INTERVAL_MS = 5000
+const INTERVAL_MS = 4500
 /**
  * 1時間+1分の間、INTERVAL_MS間隔で実行する
  * 1分はバッファで敢えて次の実行と被せる。Cloud Schedulerは1時間間隔。
  */
-const EXECUTE_COUNT = (3660 * 1000) / INTERVAL_MS
+const EXECUTE_COUNT = (3663 * 1000) / INTERVAL_MS
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(MainModule)
