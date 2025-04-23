d795a72f5608920ba18bb9d13df35cbc38c5dd82
varmil <fkmks247@gmail.com>
2025-04-23 22:14:44 +0900

    main min height change: 80vh --> 70vh

--- web/app/[locale]/(end-user)/(default)/[group]/channels/[id]/concurrent-viewers/_components/ChannelsIdConcurrentViewersTemplate.tsx
+++ web/app/[locale]/(end-user)/(default)/[group]/channels/[id]/concurrent-viewers/_components/ChannelsIdConcurrentViewersTemplate.tsx
@@ -1,10 +1,8 @@
 import { PropsWithoutRef } from 'react'
-import { getChannel } from 'apis/youtube/getChannel'
 import {
   Section,
   Sections
 } from 'features/channel/components/container/ChannelSection'
-import StreamTrendsTable from 'features/channel/components/stream-trends/StreamTrendsTable'
 import ConcurrentViewersBarChart from 'features/youtube-stats/components/bar-chart/concurrent-viewers/ConcurrentViewersBarChart'
 
 type Props = { id: string }
@@ -12,13 +10,11 @@ type Props = { id: string }
 export async function ChannelsIdConcurrentViewersTemplate({
   id
 }: PropsWithoutRef<Props>) {
-  const [channel] = await Promise.all([getChannel(id)])
-
   return (
     <Sections>
       <Section>
         <ConcurrentViewersBarChart channelId={id} className="z-10" />
-        <StreamTrendsTable channel={channel} />
+        {/* <StreamTrendsTable channel={channel} /> */}
       </Section>
     </Sections>
   )
