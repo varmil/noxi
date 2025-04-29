fbcb41baed2758de93eb9cf80b576de85ebb2927
Akihiro Yamamoto <fkmks247@gmail.com>
2025-04-29 22:33:30 +0900

    Add Manual Ads: <DisplaySquareAds> (#1875)

---
+++ web/components/ads/DisplaySquareAds.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useSearchParams } from 'next/navigation'
+import { usePathname } from 'lib/navigation'
+
+const DisplaySquareAds = () => {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    try {
+      ;((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {}
+      )
+    } catch (e) {
+      console.error(e)
+    }
+  }, [pathname, searchParams])
+
+  return (
+    <div key={pathname + searchParams.toString()}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block' }}
+        data-adtest={process.env.ENV_NAME !== 'production' ? 'on' : 'off'}
+        data-ad-client="ca-pub-4929947179869258"
+        data-ad-slot="4056949447"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
+  )
+}
+
+export default DisplaySquareAds
