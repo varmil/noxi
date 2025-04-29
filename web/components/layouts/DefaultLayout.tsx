d95fb0d788969041112f8b327b0e37c6d2e6a7fc
varmil <fkmks247@gmail.com>
2025-04-29 22:30:07 +0900

    Add Manual Ads: <DisplaySquareAds>

--- web/components/layouts/DefaultLayout.tsx
+++ web/components/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from 'react'
+import DisplaySquareAds from 'components/ads/DisplaySquareAds'
 import Aside from 'components/aside/Aside'
 import BottomNavigation from 'components/bottom-navigation/BottomNavigation'
 
@@ -11,6 +12,7 @@ export default async function DefaultLayout({ children }: PropsWithChildren) {
       {/* bottom navigation = h-14.5 なので 20.5 は pb-6 相当 */}
       <section className="z-0 relative space-y-6 pb-20.5 md:pb-8 sm:pl-14">
         {children}
+        <DisplaySquareAds />
       </section>
       {/* <CookieAgreeBanner className="z-30" /> */}
       <BottomNavigation className="z-20" />
