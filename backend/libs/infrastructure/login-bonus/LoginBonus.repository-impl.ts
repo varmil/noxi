132665cfea952d43419649cba9d2c247f469b8fb
varmil <fkmks247@gmail.com>
2025-05-09 20:32:58 +0900

    Set maxWait: 5000, timeout: 10000 for transaction

--- backend/libs/infrastructure/login-bonus/LoginBonus.repository-impl.ts
+++ backend/libs/infrastructure/login-bonus/LoginBonus.repository-impl.ts
@@ -25,72 +25,81 @@ export class LoginBonusRepositoryImpl implements LoginBonusRepository {
       const userId = data.userId.get()
       const AWARD = 1
 
-      return await this.prismaInfraService.$transaction(async tx => {
-        const lockedTicket = await tx.$queryRawUnsafe<
-          { userId: string; totalCount: number; lastClaimedAt: Date }[]
-        >(`SELECT * FROM "CheerTicket" WHERE "userId" = $1 FOR UPDATE`, userId)
+      return await this.prismaInfraService.$transaction(
+        async tx => {
+          const lockedTicket = await tx.$queryRawUnsafe<
+            { userId: string; totalCount: number; lastClaimedAt: Date }[]
+          >(
+            `SELECT * FROM "CheerTicket" WHERE "userId" = $1 FOR UPDATE`,
+            userId
+          )
 
-        let result: LoginBonusResult
-        const now = new Date()
+          let result: LoginBonusResult
+          const now = new Date()
 
-        // 登録後初回など存在しなければ insert
-        if (lockedTicket.length === 0) {
-          await tx.cheerTicket.create({
-            data: {
-              userId,
-              totalCount: AWARD,
-              lastClaimedAt: now
-            }
-          })
-          result = new LoginBonusResult({
-            eligible: true,
-            ticketsAwarded: new Awarded(AWARD),
-            totalTickets: new TotalCount(AWARD)
-          })
-        } else {
-          const { totalCount, lastClaimedAt } = lockedTicket[0]
-          const loginBonus = new LoginBonus({
-            userId: new UserId(userId),
-            lastClaimedAt: new LastClaimedAt(lastClaimedAt)
-          })
-          if (loginBonus.canClaimDaily()) {
-            await tx.cheerTicket.update({
-              where: { userId },
+          // 登録後初回など存在しなければ insert
+          if (lockedTicket.length === 0) {
+            await tx.cheerTicket.create({
               data: {
-                totalCount: { increment: AWARD },
+                userId,
+                totalCount: AWARD,
                 lastClaimedAt: now
               }
             })
             result = new LoginBonusResult({
               eligible: true,
               ticketsAwarded: new Awarded(AWARD),
-              totalTickets: new TotalCount(totalCount + AWARD)
+              totalTickets: new TotalCount(AWARD)
             })
           } else {
-            this.logger.debug('ログボはまだ受け取れません', {
+            const { totalCount, lastClaimedAt } = lockedTicket[0]
+            const loginBonus = new LoginBonus({
+              userId: new UserId(userId),
+              lastClaimedAt: new LastClaimedAt(lastClaimedAt)
+            })
+            if (loginBonus.canClaimDaily()) {
+              await tx.cheerTicket.update({
+                where: { userId },
+                data: {
+                  totalCount: { increment: AWARD },
+                  lastClaimedAt: now
+                }
+              })
+              result = new LoginBonusResult({
+                eligible: true,
+                ticketsAwarded: new Awarded(AWARD),
+                totalTickets: new TotalCount(totalCount + AWARD)
+              })
+            } else {
+              this.logger.debug('ログボはまだ受け取れません', {
+                userId,
+                lastClaimedAt,
+                now
+              })
+              return new LoginBonusResult({
+                eligible: false,
+                ticketsAwarded: new Awarded(0),
+                totalTickets: new TotalCount(totalCount)
+              })
+            }
+          }
+
+          // ログ記録
+          await tx.cheerTicketLog.create({
+            data: {
               userId,
-              lastClaimedAt,
-              now
-            })
-            return new LoginBonusResult({
-              eligible: false,
-              ticketsAwarded: new Awarded(0),
-              totalTickets: new TotalCount(totalCount)
-            })
-          }
+              count: AWARD,
+              type: 'dailyLoginBonus',
+              claimedAt: now
+            }
+          })
+
+          return result
+        },
+        {
+          maxWait: 5000,
+          timeout: 10000
         }
-
-        // ログ記録
-        await tx.cheerTicketLog.create({
-          data: {
-            userId,
-            count: AWARD,
-            type: 'dailyLoginBonus',
-            claimedAt: now
-          }
-        })
-
-        return result
-      })
+      )
     }
 }
