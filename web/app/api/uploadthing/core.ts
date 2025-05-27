06b40960f8acbe6cfbb59c6a7d520627e5ba9f36
varmil <fkmks247@gmail.com>
2025-05-27 20:59:19 +0900

    Add awaitServerData: false

--- web/app/api/uploadthing/core.ts
+++ web/app/api/uploadthing/core.ts
@@ -1,3 +1,4 @@
+import middleware from 'next-intl/middleware'
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { UploadThingError } from 'uploadthing/server'
 import { auth } from 'lib/auth'
@@ -7,16 +8,20 @@ const f = createUploadthing()
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: '1MB',
-      maxFileCount: 1
-    }
-  })
+  imageUploader: f(
+    {
+      image: {
+        /**
+         * For full list of options and defaults, see the File Route API reference
+         * @see https://docs.uploadthing.com/file-routes#route-config
+         */
+        maxFileSize: '1MB',
+        maxFileCount: 1
+      }
+    },
+    // Opt-out of waiting for server data
+    { awaitServerData: false }
+  )
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
