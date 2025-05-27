b3e68daae9e6920af84fcd4f8c8c56ef263f353e
varmil <fkmks247@gmail.com>
2025-05-27 22:48:19 +0900

    chore

--- web/app/api/uploadthing/core.ts
+++ web/app/api/uploadthing/core.ts
@@ -7,16 +7,20 @@ const f = createUploadthing()
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({
-    image: {
-      /**
-       * For full list of options and defaults, see the File Route API reference
-       * @see https://docs.uploadthing.com/file-routes#route-config
-       */
-      maxFileSize: '1MB',
-      maxFileCount: 1
-    }
-  })
+  imageUploader: f(
+    {
+      image: {
+        /**
+         * For full list of options and defaults, see the File Route API reference
+         * @see https://docs.uploadthing.com/file-routes#route-config
+         */
+        maxFileSize: '1MB',
+        maxFileCount: 1
+      }
+    },
+    // Opt-out of waiting for server data
+    { awaitServerData: false }
+  )
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
