6ae2c52500acdb8a26fb61d03b51ccf9e5086039
varmil <fkmks247@gmail.com>
2025-05-29 13:35:06 +0900

    Update stripe versions

--- backend/apps/closed-api-server/src/presentation/webhooks/stripe/webhooks-stripe.controller.ts
+++ backend/apps/closed-api-server/src/presentation/webhooks/stripe/webhooks-stripe.controller.ts
@@ -19,7 +19,7 @@ export class WebhooksStripeController {
 
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-      apiVersion: '2025-04-30.basil'
+      apiVersion: '2025-05-28.basil'
     })
   }
 
