c71b4b4cd818a07b869afc41cf5121652d8455a7
varmil <fkmks247@gmail.com>
2025-06-07 18:32:04 +0900

    chore message!

--- web/components/banners/SignupBanner.tsx
+++ web/components/banners/SignupBanner.tsx
@@ -6,11 +6,9 @@ export default function SignupBanner({ className }: { className?: string }) {
   return (
     <Alert className={className}>
       <Lightbulb className="size-5 fill-yellow-500" />
-      <AlertTitle>新ランキングのオープンβテスト開催中</AlertTitle>
+      <AlertTitle>オープンβテスト開催中</AlertTitle>
       <AlertDescription>
-        <span>
-          毎日１分でしっかり推しを応援できます。新規登録で応援チケット120枚獲得。
-        </span>
+        <span>推しを広告で支える。登録でチケット120枚</span>
         <AuthModalWithText className="text-foreground" />
       </AlertDescription>
     </Alert>
