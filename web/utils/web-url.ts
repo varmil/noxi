fd0ed68af5b72076da3e53e401bf721b45a083d4
Akihiro Yamamoto <fkmks247@gmail.com>
2025-06-08 01:20:42 +0900

    Change prod url from peakx.net to vcharts.net (#2157)

--- web/utils/web-url.ts
+++ web/utils/web-url.ts
@@ -1,11 +1,11 @@
 export function getWebUrl() {
-  const { VERCEL_ENV, VERCEL_BRANCH_URL, VERCEL_PROJECT_PRODUCTION_URL } =
-    process.env
+  const { VERCEL_ENV, VERCEL_BRANCH_URL } = process.env
 
   let URL = ''
   switch (VERCEL_ENV) {
     case 'production':
-      URL = `https://${VERCEL_PROJECT_PRODUCTION_URL ?? ''}`
+      URL = `https://www.vcharts.net`
+      // URL = `https://${VERCEL_PROJECT_PRODUCTION_URL ?? ''}`
       break
     case 'preview':
       URL = `https://${VERCEL_BRANCH_URL ?? ''}`
