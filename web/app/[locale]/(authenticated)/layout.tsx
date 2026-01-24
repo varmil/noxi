c215bede78e2e78df14378c35261b38ddef4b5a3
Akihiro Yamamoto <fkmks247@gmail.com>
2026-01-24 22:24:34 +0900

    Header重複を削除

--- web/app/[locale]/(authenticated)/layout.tsx
+++ web/app/[locale]/(authenticated)/layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react'
 import { Metadata } from 'next'
 import { setRequestLocale } from 'next-intl/server'
 import DashboardNav from 'app/[locale]/(authenticated)/dashboard/components/DashboardNav'
-import Header from 'components/header/Header'
 import DefaultLayout from 'components/layouts/DefaultLayout'
 import { routing } from 'config/i18n/routing'
 
@@ -30,8 +29,6 @@ export default async function AuthenticatedLayout({
 
   return (
     <DefaultLayout>
-      <Header className={`z-30 mb-4`} />
-
       <main className="z-0 container max-w-4xl px-4 sm:px-6 py-4">
         <h1 className="text-3xl font-bold mb-8">マイページ</h1>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
