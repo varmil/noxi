5dc2124f7604f7209e0c4fa68dfff782d4d7b204
Akihiro Yamamoto <fkmks247@gmail.com>
2026-02-06 18:36:38 +0900

    fix: 無料チケットでもエラーにならないようにバリデーションを調整

--- backend/libs/domain/hyper-chat-order/Amount.vo.ts
+++ backend/libs/domain/hyper-chat-order/Amount.vo.ts
@@ -4,7 +4,7 @@ import { NumberValueObject } from '@domain/lib'
 export class Amount extends NumberValueObject {
   @IsNotEmpty()
   @IsInt()
-  @Min(1)
+  @Min(0)
   protected readonly val: number
 
   constructor(val: number) {
