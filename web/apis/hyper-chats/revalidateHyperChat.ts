dd2e44124eb698aff145301b64f5ea9d9ea1a6e4
Akihiro Yamamoto <fkmks247@gmail.com>
2026-02-21 20:43:56 +0900

    bug: updateTag(HYPER_CHATS_LATEST) after 無料チケットで投稿

--- web/apis/hyper-chats/revalidateHyperChat.ts
+++ web/apis/hyper-chats/revalidateHyperChat.ts
@@ -1,10 +1,15 @@
 'use server'
 
 import { updateTag } from 'next/cache'
-import { getHyperChatTag, HYPER_TRAINS_ACTIVE } from 'apis/tags/revalidate-tags'
+import {
+  getHyperChatTag,
+  HYPER_CHATS_LATEST,
+  HYPER_TRAINS_ACTIVE
+} from 'apis/tags/revalidate-tags'
 
 /** 指定されたchannelIdのハイパーチャットキャッシュを無効化する */
 export async function revalidateHyperChat(channelId: string): Promise<void> {
   updateTag(getHyperChatTag(channelId))
   updateTag(HYPER_TRAINS_ACTIVE)
+  updateTag(HYPER_CHATS_LATEST)
 }
